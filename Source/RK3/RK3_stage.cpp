65da545435df786e4172afc556d7244d364e118a
Brian J Gaudet <gaud039@constance01.pnl.gov>
2021-07-08 14:52:26 -0700

    The gravitational constant is set to be a positive number, but its treatment in the vertical momentum equation assumes it is negative. The bug fix is to insert a negative sign when grav_gpu is defined.

--- Source/RK3/RK3_stage.cpp
+++ Source/RK3/RK3_stage.cpp
@@ -49,7 +49,10 @@ void RK3_stage  (MultiFab& cons_old,  MultiFab& cons_upd,
     // **************************************************************************************
     // Deal with gravity
     Real gravity = solverChoice.use_gravity? CONST_GRAV: 0.0;
-    const    Array<Real,AMREX_SPACEDIM> grav{0.0, 0.0, gravity};
+    // CONST_GRAV is a positive constant, but application of grav_gpu to the vertical momentum
+    // tendency assumes this quantity is negative.
+    //    const    Array<Real,AMREX_SPACEDIM> grav{0.0, 0.0, gravity};
+    const    Array<Real,AMREX_SPACEDIM> grav{0.0, 0.0, -gravity};
     const GpuArray<Real,AMREX_SPACEDIM> grav_gpu{grav[0], grav[1], grav[2]};
 
     // **************************************************************************************
