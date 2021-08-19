66a9865df65157581b9b99c05aa1eebd7783d78b
Pankaj Jha <jha3@llnl.gov>
2021-08-19 05:32:23 -0700

    Update ABL and Channel LES problems to account for initial perturbations

--- Exec/ABL/prob.cpp
+++ Exec/ABL/prob.cpp
@@ -3,8 +3,15 @@
 namespace ProbParm {
 AMREX_GPU_DEVICE_MANAGED amrex::Real rho_0 = 0.0; // left density (g/cc)
 AMREX_GPU_DEVICE_MANAGED amrex::Real rhoe_0;
-AMREX_GPU_DEVICE_MANAGED amrex::Real T_0 = 1.0;
+AMREX_GPU_DEVICE_MANAGED amrex::Real Theta_0 = 1.0;
 AMREX_GPU_DEVICE_MANAGED amrex::Real A_0 = 1.0;
+
+AMREX_GPU_DEVICE_MANAGED amrex::Real U0 = 10.0;
+AMREX_GPU_DEVICE_MANAGED amrex::Real V0 = 0.0;
+AMREX_GPU_DEVICE_MANAGED amrex::Real W0 = 0.0;
+AMREX_GPU_DEVICE_MANAGED amrex::Real U0_Pert_Mag = 1.0;
+AMREX_GPU_DEVICE_MANAGED amrex::Real V0_Pert_Mag = 1.0;
+AMREX_GPU_DEVICE_MANAGED amrex::Real W0_Pert_Mag = 0.0;
 } // namespace ProbParm
 
 void
@@ -24,7 +31,14 @@ amrex_probinit(
   // Parse params
   amrex::ParmParse pp("prob");
   pp.query("rho_0", ProbParm::rho_0);
-  pp.query("T_0", ProbParm::T_0);
+  pp.query("T_0", ProbParm::Theta_0);
   pp.query("A_0", ProbParm::A_0);
+
+  pp.query("U0", ProbParm::U0);
+  pp.query("V0", ProbParm::V0);
+  pp.query("W0", ProbParm::W0);
+  pp.query("U0_Pert_Mag", ProbParm::U0_Pert_Mag);
+  pp.query("V0_Pert_Mag", ProbParm::V0_Pert_Mag);
+  pp.query("W0_Pert_Mag", ProbParm::W0_Pert_Mag);
 }
 }
