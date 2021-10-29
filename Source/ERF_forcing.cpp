be145fff01b26e083c2eef0b58529fe33c17d279
Ann Almgren <asalmgren@lbl.gov>
2021-10-29 13:21:40 -0700

    add coriolis forcing

--- Source/ERF_forcing.cpp
+++ Source/ERF_forcing.cpp
@@ -7,27 +7,36 @@
 using namespace amrex;
 
 void
-ERF::build_geostrophic_forcing()
+ERF::build_coriolis_forcings()
 {
-    amrex::ParmParse pp("erf");
+    if (!use_coriolis) return;
 
-    // Read in the geostrophic wind -- we only use this to construct
-    //     the forcing term so no need to keep it
-    amrex::Vector<amrex::Real> abl_geo_wind(3);
-    pp.queryarr("abl_geo_wind",abl_geo_wind);
+    amrex::ParmParse pp("erf");
 
     // Read the rotational time period (in seconds)
     amrex::Real rot_time_period = 86400.0;
     pp.query("rotational_time_period", rot_time_period);
 
-    Real coriolis_factor = 2.0 * 2.0 * M_PI / rot_time_period;
-    amrex::Print() << "Geostrophic forcing: Coriolis factor = "
-                   << coriolis_factor << std::endl;
+    coriolis_factor = 2.0 * 2.0 * M_PI / rot_time_period;
+    amrex::Print() << "Coriolis factor = " << coriolis_factor << std::endl;
 
     amrex::Real latitude = 90.0;
     pp.query("latitude", latitude);
     AMREX_ALWAYS_ASSERT(amrex::Math::abs(latitude - 90.0) < 1.e-12);
 
-    abl_geo_forcing =
-       {-coriolis_factor * abl_geo_wind[1], coriolis_factor * abl_geo_wind[0], 0.0};
+    // Convert to radians 
+    latitude *= (M_PI/180.);
+    sinphi = std::sin(latitude);
+    cosphi = std::cos(latitude);
+
+    if (abl_driver_type == "GeostrophicWind")
+    {
+        // Read in the geostrophic wind -- we only use this to construct
+        //     the forcing term so no need to keep it
+        amrex::Vector<amrex::Real> abl_geo_wind(3);
+        pp.queryarr("abl_geo_wind",abl_geo_wind);
+
+        abl_geo_forcing =
+           {-coriolis_factor * abl_geo_wind[1], coriolis_factor * abl_geo_wind[0], 0.0};
+    }
 }
