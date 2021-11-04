09a2e0bd0d6ef25acf92866883d03b1ae0b41d43
etpalmer63 <etpalmer@math.sc.edu>
2021-11-04 16:44:59 -0400

    Add single script to build docs

--- Docs/BuildDocs.sh
+++ Docs/BuildDocs.sh
@@ -3,6 +3,11 @@
 
 # This short script builds both the doxygen and sphinx documentation
 
+# Define pretty colors
+YEL='\033[0;33m'
+GRN='\033[1;32m'
+NC='\033[0m'
+
 
 # Build doxygen documents -- configuration parameters contained in Doxyfile.in
 doxygen Doxyfile.in
@@ -17,3 +22,7 @@ make html
 
 # After build, sphinx html files can be found in `sphinx_doc/_build/html/`
 
+echo "" 
+echo -e "-- ${GRN}Building Docs Complete${NC} --"
+echo -e "See ${YEL}doxygen_output/html/${NC} for doxygen html files"
+echo -e "See ${YEL}sphinx_doc/_build/html/${NC} for sphinx html files"
