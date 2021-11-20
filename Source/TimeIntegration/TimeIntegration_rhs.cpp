41644bf0f9f020fc60efc97b8ad20a1fd386efe0
Eliot Quon <eliot.quon@nrel.gov>
2021-11-20 13:56:59 -0700

    Fix coriolis bug when calculating local v

--- Source/TimeIntegration/TimeIntegration_rhs.cpp
+++ Source/TimeIntegration/TimeIntegration_rhs.cpp
@@ -194,7 +194,7 @@ void erf_rhs (int level,
             // Add Coriolis forcing (that assumes east is +x, north is +y)
             if (solverChoice.use_coriolis)
             {
-                Real rho_v_loc = 0.25 * (rho_u(i,j+1,k) + rho_u(i,j,k) + rho_u(i-1,j+1,k) + rho_u(i-1,j,k));
+                Real rho_v_loc = 0.25 * (rho_v(i,j+1,k) + rho_v(i,j,k) + rho_v(i-1,j+1,k) + rho_v(i-1,j,k));
                 Real rho_w_loc = 0.25 * (rho_w(i,j,k+1) + rho_w(i,j,k) + rho_w(i,j-1,k+1) + rho_w(i,j-1,k));
                 rho_u_rhs(i, j, k) += solverChoice.coriolis_factor *
                         (rho_v_loc * solverChoice.sinphi - rho_w_loc * solverChoice.cosphi);
