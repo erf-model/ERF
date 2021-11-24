1) fix the strain rate computation at no-slip walls 2) Add Poiseuille flow test 3) Fix Couette flow test (shear in z-direction)

Exec/CouetteFlow/plot_couette_flow_evolution.py
@@ -7,7 +7,8 @@ import matplotlib.pyplot as plt
 import yt
 from couette_helpers import read_input
 
-outputs = [100, 200, 1300, 2600, 12800]
+#outputs = [100, 200, 1300, 2600, 12800]
+outputs = [10,20,30,40,50]
 Utop = 2.0
 
 try:
@@ -17,19 +18,19 @@ except IndexError:
 pltfiles = [os.path.join(results_dir,f'plt{idx:05d}') for idx in outputs]
 assert all([os.path.isdir(pltfile) for pltfile in pltfiles])
 
-pp = read_input(os.path.join(results_dir,'inputs_ex'))
+pp = read_input(os.path.join(results_dir,'inputs_couette'))
 dt = pp['erf.fixed_dt']
 print('dt=',dt)
 nu = pp['erf.dynamicViscosity'] / pp['prob.rho_0']
 print('nu=',nu)
-ymin = float(pp['geometry.prob_lo'][1])
-ymax = float(pp['geometry.prob_hi'][1])
-ny = int(pp['amr.n_cell'][1])
-h = ymax - ymin
+zmin = float(pp['geometry.prob_lo'][2])
+zmax = float(pp['geometry.prob_hi'][2])
+nz = int(pp['amr.n_cell'][2])
+h = zmax - zmin
 print('h=',h)
 
-yface = np.linspace(ymin,ymax,ny+1)
-y = (yface[1:] + yface[:-1]) / 2
+zface = np.linspace(zmin,zmax,nz+1)
+z = (zface[1:] + zface[:-1]) / 2
 
 nondimtimes = nu * np.array(outputs)*dt / h**2
 
@@ -42,11 +43,11 @@ for ndt,pltfile in zip(nondimtimes,pltfiles):
     pf = yt.load(os.path.join(results_dir, pltfile))
     level0 = pf.covering_grid(level=0, left_edge=pf.domain_left_edge, dims=pf.domain_dimensions)
     u = level0['x_velocity'][0,:,0]
-    ax.plot(u/Utop, y/h, label=ndt)
+    ax.plot(u/Utop, z/h, label=ndt)
 ax.set_xlim((0,1))
 ax.set_ylim((0,1))
 lgd = ax.legend(title=r'$\nu t/h^2$',title_fontsize='large')
-ax.set_ylabel(r'$y/h$',fontsize='x-large')
+ax.set_ylabel(r'$z/h$',fontsize='x-large')
 ax.set_xlabel(r'$u/U$',fontsize='x-large')
 ax.grid()
 fig.savefig('Couette_profiles.png',bbox_inches='tight')
@@ -54,6 +55,6 @@ fig.savefig('Couette_profiles.png',bbox_inches='tight')
 #
 # check error at last step
 #
-err = u.value - Utop*y/h
+err = u.value - Utop*z/h
 print('MAE',np.mean(np.abs(err)))
 print('RMSE', np.sqrt(np.mean(err*err)))

Exec/EkmanSpiral/check_convergence.py
@@ -38,12 +38,12 @@ def load_soln(pltfile):
 #pltfile = 'plt00000'
 pltfile = 'plt00010'
 results = {
-    25.0: load_soln(f'convergence/dz25.0/{pltfile}'),
-    12.5: load_soln(f'convergence/dz12.5/{pltfile}'),
-    6.25: load_soln(f'convergence/dz6.25/{pltfile}'),
-    3.125: load_soln(f'convergence/dz3.125/{pltfile}'),
-    1.5625: load_soln(f'convergence/dz1.5625/{pltfile}'),
-    0.78125: load_soln(f'convergence/dz0.78125/{pltfile}'),
+    25.0: load_soln(f'convergence/dz25.0/plt00010'),
+    12.5: load_soln(f'convergence/dz12.5/plt00020'),
+    6.25: load_soln(f'convergence/dz6.25/plt00040'),
+    3.125: load_soln(f'convergence/dz3.125/plt00080'),
+    1.5625: load_soln(f'convergence/dz1.5625/plt00160'),
+    0.78125: load_soln(f'convergence/dz0.78125/plt00320'),
 }
 
 #
@@ -99,8 +99,15 @@ errors = pd.DataFrame({
 print(errors)
 
 fig,ax = plt.subplots()
-ax.semilogy(errors.index, errors['uerr'], label='u-error')
-ax.semilogy(errors.index, errors['verr'], label='v-error')
+ax.loglog(errors.index, errors['uerr'], '-o', label='u-error')
+ax.loglog(errors.index, errors['verr'], '-o', label='v-error')
+
+y1 = errors['uerr'].iloc[0]
+y2 = errors['uerr'].iloc[0]/1024.
+x = np.array([0.78125, 25.0])
+y = np.array([y2,y1])
+ax.loglog(x,y, '-', label='second-order')
+
 ax.legend(loc='upper left',fontsize='large')
 ax.set_xlabel(r'$\Delta z$ [m]',fontsize='x-large')
 ax.set_ylabel(r'$L_2$ error [m/s]',fontsize='x-large')