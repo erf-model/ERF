2f0c73a0c121a15c0c5c68938f4aa33992c109d0
Ann Almgren <asalmgren@lbl.gov>
2021-12-09 17:11:11 -0800

    make rhotheta const

--- Source/ERF.cpp
+++ Source/ERF.cpp
@@ -924,11 +924,12 @@ ERF::derive(const std::string& name, amrex::Real time, int ngrow)
           const Array4<Real const>& sdat = S_new.array(mfi);
           const Array4<Real>& derdat = (*derive_dat).array(mfi);
           amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
-              Real rhotheta = sdat(i,j,k,RhoTheta_comp);
+              const Real rhotheta = sdat(i,j,k,RhoTheta_comp);
               derdat(i, j, k) = getPgivenRTh(rhotheta) - d_pres_hse_lev[k];
           });
       }
       return derive_dat;
+
   } else {
      return AmrLevel::derive(name, time, ngrow);
   }
