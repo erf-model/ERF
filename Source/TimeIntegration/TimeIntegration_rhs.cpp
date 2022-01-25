4255878d6bacc1b7ddc6b1f7d3fe8c2d1404e3a4
Xingqiu Yuan <xyuan@anl.gov>
2022-01-25 11:51:25 -0500

    fix the buoyance source term

--- Source/TimeIntegration/TimeIntegration_rhs.cpp
+++ Source/TimeIntegration/TimeIntegration_rhs.cpp
@@ -168,22 +168,22 @@ void erf_rhs (int level,
                 Real theta = cell_prim(i,j,k,PrimTheta_comp);
                 cell_rhs(i, j, k, n) -= dptr_rayleigh_tau[k] * (theta - dptr_rayleigh_thetabar[k]) * cell_data(i,j,k,Rho_comp);
             }
-            
-            // Add Buoyance Source             
+
+            // Add Buoyance Source
             if (l_use_deardorff && n == RhoKE_comp)
             {
+                Real rho     = cell_prim(i,j,k,Rho_comp);
                 Real theta   = cell_prim(i,j,k,PrimTheta_comp);
                 Real dtheta  = 0.5*(cell_prim(i,j,k+1,PrimTheta_comp)-cell_prim(i,j,k-1,PrimTheta_comp));
                 Real KE      = cell_prim(i,j,k,PrimKE_comp);
-                Real delta_s = std::sqrt(dx[0]*dx[0]+dx[1]*dx[1]+dx[2]*dx[2]);
                 Real length  = 0.;
                 if (dtheta <= 0.) {
-                   length = delta_s;
+                   length = l_Delta;
                 } else {
                    length = 0.76*std::sqrt(KE)*(grav_gpu[2]/theta)*dtheta*dxInv[2];
                 }
                 Real KM   = 0.1*(1.+2.*length/delta_s)*std::sqrt(KE);
-                cell_rhs(i, j, k, n) += grav_gpu[2]*KM*dtheta*dxInv[2];
+                cell_rhs(i, j, k, n) += rho*grav_gpu[2]*KM*dtheta*dxInv[2];
             }
 
             if (l_use_deardorff && n == RhoKE_comp)
