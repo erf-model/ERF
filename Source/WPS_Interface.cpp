701cea08ef9823b544cc41778a02ce9262b321b0
Pankaj Jha <jha3@llnl.gov>
2022-02-03 09:59:47 -0800

    Add the skeleton of NetCDF to ERF/multifab interface

--- Source/WPS_Interface.cpp
+++ Source/WPS_Interface.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "WPS_Interface.H"
+
+void Metgrid_Output::readMetgridOutputFiles(const std::string &metgrid_files_loc) {
+
+}
+
+void Metgrid_Output::convertNetCDFToPlotfiles() {
+
+}
+
+void ERF_IC_BC::readMetgridNetCDFFiles() {
+
+}
+
+void ERF_IC_BC::convertNetCDFBOundaryToMultifab() {
+
+}
+
+void ERF_IC_BC::convertNetCDFICToMultifab() {
+
+}
