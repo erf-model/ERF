3fd8fb9bbb88ec016f3d32d9a4d93d939bf7b2bf
Pankaj Jha <jha3@llnl.gov>
2022-03-12 23:07:46 -0800

    Add the portion of code from erf_init_prob of EkmanSpiral to erf_init_from_metdata of ERF.cpp

--- Source/ERF.cpp
+++ Source/ERF.cpp
@@ -806,4 +806,85 @@ erf_init_from_metdata(
     });
      */
 #endif
+
+    // The code piece below has been copied from erf_init_prob for Exec/EkmanSpriral
+    //
+    struct ProbParm {
+        amrex::Real rho_0 = 1.0;
+        amrex::Real Theta_0 = 300.0;
+        amrex::Real V_0 = 1.0;
+    };
+    ProbParm parms;
+
+    amrex::ParallelFor(bx, [=, parms=parms] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+        // Geometry
+        //const amrex::Real* prob_lo = geomdata.ProbLo();
+        //const auto dx = geomdata.CellSize();
+        //const amrex::Real x = prob_lo[0] + (i + 0.5) * dx[0];
+        //const amrex::Real y = prob_lo[1] + (j + 0.5) * dx[1];
+        //const amrex::Real z = prob_lo[2] + (k + 0.5) * dx[2];
+
+        // Set the density
+        state(i, j, k, Rho_comp) = parms.rho_0;
+
+        // Initial potential temperature (Actually rho*theta)
+        state(i, j, k, RhoTheta_comp) = parms.rho_0 * parms.Theta_0;
+
+
+
+        // Set scalar = 0 everywhere
+        state(i, j, k, RhoScalar_comp) = 0.0;
+    });
+
+    amrex::ParmParse pp("erf");
+    amrex::Real rot_time_period;
+    pp.get("rotational_time_period", rot_time_period);
+    amrex::Real coriolis_factor = 4.0 * PI / rot_time_period;
+
+    amrex::Real Az;
+    pp.get("dynamicViscosity", Az); // dynamic viscosity [kg-m/s]
+    Az = Az / parms.rho_0; // kinematic viscosity [m^2/s]
+
+    amrex::Vector<amrex::Real> abl_geo_wind(3);
+    pp.queryarr("abl_geo_wind",abl_geo_wind);
+    AMREX_ALWAYS_ASSERT_WITH_MESSAGE(
+            (amrex::Math::abs(abl_geo_wind[1]) < 1.0e-15) &&
+            (amrex::Math::abs(abl_geo_wind[2]) < 1.0e-15),
+            "Ekman Spiral uses geostrophic forcing of the form (V_0, 0, 0)");
+    const amrex::Real u_0 = abl_geo_wind[0];
+
+    const amrex::Real DE = std::sqrt(2.0 * Az / coriolis_factor);
+    //amrex::Print() << "Ekman depth = " << DE << " m" << std::endl;
+    const amrex::Real a = 1.0 / DE;
+
+    // Construct a box that is on x-faces
+    const amrex::Box& xbx = amrex::surroundingNodes(bx,0);
+    // Set the x-velocity
+    amrex::ParallelFor(xbx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+
+        const auto dx = geomdata.CellSize();
+        const amrex::Real z = (k + 0.5) * dx[2];
+
+        // Set the x-velocity
+        x_vel(i, j, k) = u_0 * (1.0 - std::exp(-a * z) * std::cos(-a * z));
+    });
+
+    // Construct a box that is on y-faces
+    const amrex::Box& ybx = amrex::surroundingNodes(bx,1);
+    // Set the y-velocity
+    amrex::ParallelFor(ybx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+
+        const auto dx = geomdata.CellSize();
+        const amrex::Real z = (k + 0.5) * dx[2];
+
+        // Set the y-velocity
+        y_vel(i, j, k) = -u_0 * std::exp(-a * z) * std::sin(-a * z);
+    });
+
+    // Construct a box that is on z-faces
+    const amrex::Box& zbx = amrex::surroundingNodes(bx,2);
+    // Set the z-velocity
+    amrex::ParallelFor(zbx, [=, parms=parms] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+        z_vel(i, j, k) = 0.0;
+    });
 }
