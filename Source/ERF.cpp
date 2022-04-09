da3906905e723cfd1c5b684c40695f5d5ff620f8
Ann Almgren <asalmgren@lbl.gov>
2022-04-09 13:29:04 -0700

    trailing white space

--- Source/ERF.cpp
+++ Source/ERF.cpp
@@ -665,7 +665,7 @@ ERF::ReadParameters ()
                 amrex::Abort("Dt is over-specfied");
             } else {
                 fixed_mri_dt_ratio = fixed_dt / fixed_fast_dt;
-                if (fixed_mri_dt_ratio%2 != 0) 
+                if (fixed_mri_dt_ratio%2 != 0)
                     fixed_mri_dt_ratio += 1; // This ratio must be even
             }
         }
