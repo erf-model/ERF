fa6e4c05490535a27c33f095d9f730535f11ff1b
Pankaj Jha <jha3@llnl.gov>
2022-04-26 12:56:06 -0700

    Framework for reading lateral boundary data output from real.exe

--- Source/IO/NCBuildFABs.cpp
+++ Source/IO/NCBuildFABs.cpp
@@ -7,6 +7,7 @@
 #include "NCInterface.H"
 #include "AMReX_Print.H"
 
+
 //
 // This is the example function that demonstrate how to build the MultiFab using the
 // NetCDF variables.
@@ -83,6 +84,8 @@ BuildFABsFromWRFInputFile(const std::string &fname,
 
         for (int i = 0; i < nc_var_names.size(); i++)
         {
+            amrex::Print() << "Building FAB for the the NetCDF variable : " << nc_var_names[i] << std::endl;
+
             // Build the box first using the shape information
             amrex::IntVect smallEnd{0, 0, 0};
             amrex::IntVect bigEnd{0, 0, 0};
@@ -129,6 +132,72 @@ BuildFABsFromWRFInputFile(const std::string &fname,
                 auto dataPtr = fab_vars[i]->dataPtr(k);
                 for (int n(0); n < num_pts; ++n) {
                     *(dataPtr+n) = static_cast<amrex::Real>(*(arrays[i].get_data()+n));
+//                    if (n % 5000 == 0)
+//                        //amrex::Print() << "n: " << n << ", data[n]: " << *(dataPtr+n) << std::endl;
+//                        amrex::Print() << "n: " << n << ", data[n]: " << *(arrays[i].get_data()+n) << std::endl;
+                }
+            }
+        }
+    } // if IOProcessor
+}
+
+void
+BuildFABsFromWRFBdyFile(const std::string &fname, amrex::Vector<std::string> nc_var_names,
+                             amrex::Vector<FArrayBox *> fab_vars, Vector<enum NC_Data_Dims_Type> NC_dim_types) {
+    if (amrex::ParallelDescriptor::IOProcessor())
+    {
+        using RARRAY = NDArray<float>;
+        amrex::Vector<RARRAY> arrays(nc_var_names.size());
+        ReadWRFBdyFile(fname, nc_var_names, arrays);
+
+        for (int i = 0; i < nc_var_names.size(); i++)
+        {
+            amrex::Print() << "Building FAB for the the NetCDF variable : " << nc_var_names[i] << std::endl;
+
+            auto shape_data = arrays[i].get_vshape();
+            // Need to decide what would be the best data structure to hold Time X BdyWidth X BT X SN or Time X BdyWidth X BT X WE
+
+            // Build the box first using the shape information
+//            amrex::IntVect smallEnd{0, 0, 0};
+//            amrex::IntVect bigEnd{0, 0, 0};
+//
+//            switch (NC_dim_types[i]) {
+//                case NC_Data_Dims_Type::Time_BdyWidth_BT_SN:
+//                    bigEnd[0] = 0;
+//                    bigEnd[1] = arrays[i].get_vshape()[3] - 1;
+//                    bigEnd[2] = arrays[i].get_vshape()[2] - 1;
+//                    break;
+//                case NC_Data_Dims_Type::Time_BdyWidth_BT_WE:
+//                    bigEnd[0] = arrays[i].get_vshape()[3] - 1;
+//                    bigEnd[1] = 0;
+//                    bigEnd[2] = arrays[i].get_vshape()[2] - 1;
+//                    break;
+//                default:
+//                    amrex::Error("Unrecognized NetCDF data dimensions type");
+//            }
+
+            int tot_size = shape_data[0]*shape_data[1]*shape_data[2]*shape_data[3];
+            // amrex::Print() << "Total points in the box constructed from netCDF variable: " << tot_size << std::endl;
+
+//            amrex::IntVect boxType{0, 0, 0};
+//            if (nc_var_names[i] == "U_BXS" || nc_var_names[i] == "U_BXE" || nc_var_names[i] == "U_BYS" || nc_var_names[i] == "U_BYE")
+//                boxType = amrex::IntVect(1,0,0);
+//            amrex::Box bx = amrex::Box(smallEnd, bigEnd, boxType);
+
+            auto bx_fab = fab_vars[i]->box(); // This is not doing what it's supposed to be doing
+            //AMREX_ALWAYS_ASSERT(bx == fab_vars[i]->box());
+            int ncomp  = 1;
+            //auto num_pts = fab_vars[i]->box().numPts(); // This is not doing what it's supposed to be doing
+            auto num_pts = tot_size;
+
+            // Commented out this. Needs to be fixed
+            for (int k(0); k < ncomp; ++k) {
+                //auto dataPtr = fab_vars[i]->dataPtr(k); // This is not pointing to the right location
+                for (int n(0); n < num_pts; ++n) {
+                    //*(dataPtr+n) = static_cast<amrex::Real>(*(arrays[i].get_data()+n));
+//                    if (n % 5000 == 0)
+//                        //amrex::Print() << "n: " << n << ", data[n]: " << *(dataPtr+n) << std::endl;
+//                        amrex::Print() << "n: " << n << ", data[n]: " << *(arrays[i].get_data()+n) << std::endl;
                 }
             }
         }
