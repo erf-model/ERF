66fde92d50b6a4660423369ac688325606e08e93
Michael Sanders <msanders@gaira.dhcp.lbl.gov>
2022-07-18 14:52:21 -0700

    Uncomment functions to allow parallel netCDF

--- Source/IO/NCInterface.cpp
+++ Source/IO/NCInterface.cpp
@@ -313,12 +313,12 @@ void NCVar::get_attr(const std::string& name, std::vector<int>& values) const
 }
 
 //Uncomment for parallel NetCDF
-/*
 void NCVar::par_access(const int cmode) const
 {
     check_nc_error(nc_var_par_access(ncid, varid, cmode));
 }
-*/
+
+
 std::string NCGroup::name() const
 {
     size_t nlen;
@@ -578,8 +578,8 @@ NCFile NCFile::open(const std::string& name, const int cmode)
     check_nc_error(nc_open(name.data(), cmode, &ncid));
     return NCFile(ncid);
 }
+
 //Uncomment for parallel NetCDF
-/*
 NCFile NCFile::create_par(
     const std::string& name, const int cmode, MPI_Comm comm, MPI_Info info)
 {
@@ -596,7 +596,7 @@ NCFile NCFile::open_par(
     check_nc_error(nc_open_par(name.data(), cmode, comm, info, &ncid));
     return NCFile(ncid);
 }
-*/
+
 NCFile::~NCFile()
 {
     if (is_open) check_nc_error(nc_close(ncid));
