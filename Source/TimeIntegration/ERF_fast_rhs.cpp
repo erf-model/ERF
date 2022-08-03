erf-model/ERF

commit 96ddb86b7b5238784f6fd009c1606f7dfa719283

Reduce number of Copy calls in the fast RHS (#619)
* WIP optimize fast RHS.

* Fix runtime issue.

Source/TimeIntegration/ERF_fast_rhs.cpp
@@ -61,30 +61,9 @@ void erf_fast_rhs (int level,
     MultiFab Delta_rho  (            ba                , dm, 1, 1);
     MultiFab Delta_rho_theta(        ba                , dm, 1, 1);
 
-    // Create old_drho_u/v/w/theta  = U'', V'', W'', Theta'' in the docs
-    // Note that we do the Copy and Subtract including one ghost cell
-    //    so that we don't have to fill ghost cells of the new MultiFabs
-    MultiFab::Copy(Delta_rho_u    , S_data[IntVar::xmom], 0, 0, 1, 1);
-    MultiFab::Copy(Delta_rho_v    , S_data[IntVar::ymom], 0, 0, 1, 1);
-    MultiFab::Copy(Delta_rho_w    , S_data[IntVar::zmom], 0, 0, 1, IntVect(1,1,0));
-    MultiFab::Copy(Delta_rho      , S_data[IntVar::cons], Rho_comp     , 0, 1,1);
-    MultiFab::Copy(Delta_rho_theta, S_data[IntVar::cons], RhoTheta_comp, 0, 1,1);
-
-    MultiFab::Subtract(Delta_rho_u    , S_stage_data[IntVar::xmom], 0, 0, 1, 1);
-    MultiFab::Subtract(Delta_rho_v    , S_stage_data[IntVar::ymom], 0, 0, 1, 1);
-    MultiFab::Subtract(Delta_rho_w    , S_stage_data[IntVar::zmom], 0, 0, 1, IntVect(1,1,0));
-    MultiFab::Subtract(Delta_rho      , S_stage_data[IntVar::cons], Rho_comp     , 0, 1, 1);
-    MultiFab::Subtract(Delta_rho_theta, S_stage_data[IntVar::cons], RhoTheta_comp, 0, 1, 1);
-
-    amrex::MultiFab New_rho_u(convert(ba,IntVect(1,0,0)), dm, 1, 1);
-    amrex::MultiFab New_rho_v(convert(ba,IntVect(0,1,0)), dm, 1, 1);
-    amrex::MultiFab New_rho_w(convert(ba,IntVect(0,0,1)), dm, 1, 1);
-
-    // Initialize New_rho_u/v/w to Delta_rho_u/v/w so that
-    // the ghost cells in New_rho_u/v/w will match old_drho_u/v/w
-    MultiFab::Copy(New_rho_u, Delta_rho_u, 0, 0, 1, 1);
-    MultiFab::Copy(New_rho_v, Delta_rho_v, 0, 0, 1, 1);
-    MultiFab::Copy(New_rho_w, Delta_rho_w, 0, 0, 1, IntVect(1,1,0));
+    MultiFab New_rho_u(convert(ba,IntVect(1,0,0)), dm, 1, 1);
+    MultiFab New_rho_v(convert(ba,IntVect(0,1,0)), dm, 1, 1);
+    MultiFab New_rho_w(convert(ba,IntVect(0,0,1)), dm, 1, 1);
 
     // *************************************************************************
     // Set gravity as a vector
@@ -110,6 +89,8 @@ void erf_fast_rhs (int level,
 #ifdef _OPENMP
 #pragma omp parallel if (amrex::Gpu::notInLaunchRegion())
 #endif
+    {
+
     for ( MFIter mfi(S_stage_data[IntVar::cons],TilingIfNotGPU()); mfi.isValid(); ++mfi) {
 
         const Box& bx = mfi.tilebox();
@@ -130,6 +111,9 @@ void erf_fast_rhs (int level,
 
         const Array4<      Real> & fast_rhs_cell = S_rhs[IntVar::cons].array(mfi);
         const Array4<const Real> & cell_stage    = S_stage_data[IntVar::cons].const_array(mfi);
+        const Array4<const Real> & cell_stage_xmom  = S_stage_data[IntVar::xmom].const_array(mfi);
+        const Array4<const Real> & cell_stage_ymom  = S_stage_data[IntVar::ymom].const_array(mfi);
+        const Array4<const Real> & cell_stage_zmom  = S_stage_data[IntVar::zmom].const_array(mfi);
         const Array4<const Real> & prim          = S_stage_prim.const_array(mfi);
 
         const Array4<Real>& old_drho_u     = Delta_rho_u.array(mfi);
@@ -161,6 +145,9 @@ void erf_fast_rhs (int level,
         const Array4<      Real>& advflux_z = advflux[2].array(mfi);
 
         const Array4<const Real>& cur_data = S_data[IntVar::cons].const_array(mfi);
+        const Array4<const Real>& cur_data_xmom = S_data[IntVar::xmom].const_array(mfi);
+        const Array4<const Real>& cur_data_ymom = S_data[IntVar::ymom].const_array(mfi);
+        const Array4<const Real>& cur_data_zmom = S_data[IntVar::zmom].const_array(mfi);
         const Array4<const Real>& old_data = S_scratch[IntVar::cons].const_array(mfi);
 
         // These store the advection momenta which we will use to update the slow variables
@@ -176,10 +163,35 @@ void erf_fast_rhs (int level,
 
         const Array4<Real>& extrap_arr = extrap.array(mfi);
 
-        const Box& gbx = mfi.growntilebox(1);
+        // Create old_drho_u/v/w/theta  = U'', V'', W'', Theta'' in the docs
+        // Note that we do the Copy and Subtract including one ghost cell
+        //    so that we don't have to fill ghost cells of the new MultiFabs
+        // Initialize New_rho_u/v/w to Delta_rho_u/v/w so that
+        // the ghost cells in New_rho_u/v/w will match old_drho_u/v/w
+        const Box& gbx  = mfi.growntilebox(1);
+        const Box& gtbx = mfi.nodaltilebox(0).grow(1);
+        const Box& gtby = mfi.nodaltilebox(1).grow(1);
+        const Box& gtbz = mfi.nodaltilebox(2).grow(IntVect(1,1,0));
+
         amrex::ParallelFor(gbx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept {
-           extrap_arr(i,j,k) = old_drho_theta(i,j,k) + beta_d * (
+            old_drho(i,j,k) = cur_data(i,j,k,Rho_comp) - cell_stage(i,j,k,Rho_comp);
+            old_drho_theta(i,j,k) = cur_data(i,j,k,RhoTheta_comp) - cell_stage(i,j,k,RhoTheta_comp);
+            extrap_arr(i,j,k) = old_drho_theta(i,j,k) + beta_d * (
               (cur_data(i,j  ,k,RhoTheta_comp) - old_data(i,j  ,k,RhoTheta_comp)));
+        });
+
+        amrex::ParallelFor(gtbx, gtby, gtbz,
+        [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept {
+            old_drho_u(i,j,k) = cur_data_xmom(i,j,k) - cell_stage_xmom(i,j,k);
+            new_drho_u(i,j,k) = old_drho_u(i,j,k);
+        },
+        [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept {
+            old_drho_v(i,j,k) = cur_data_ymom(i,j,k) - cell_stage_ymom(i,j,k);
+            new_drho_v(i,j,k) = old_drho_v(i,j,k);
+        },
+        [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept {
+            old_drho_w(i,j,k) = cur_data_zmom(i,j,k) - cell_stage_zmom(i,j,k);
+            new_drho_w(i,j,k) = old_drho_w(i,j,k);
         });
 
         // *********************************************************************
@@ -566,4 +578,5 @@ void erf_fast_rhs (int level,
         });
 
     } // mfi
+    }
 }

commit cbc977b671462e14a7c82eb66cf720970ee6e66a

Fix Box error introduced in 96ddb86b that shows up when running with terrain by declaring Box objects instead of references to a Box when using mfi.nodaltilebox...

Source/TimeIntegration/ERF_fast_rhs.cpp
@@ -168,10 +168,10 @@ void erf_fast_rhs (int level,
         //    so that we don't have to fill ghost cells of the new MultiFabs
         // Initialize New_rho_u/v/w to Delta_rho_u/v/w so that
         // the ghost cells in New_rho_u/v/w will match old_drho_u/v/w
-        const Box& gbx  = mfi.growntilebox(1);
-        const Box& gtbx = mfi.nodaltilebox(0).grow(1);
-        const Box& gtby = mfi.nodaltilebox(1).grow(1);
-        const Box& gtbz = mfi.nodaltilebox(2).grow(IntVect(1,1,0));
+        const Box gbx  = mfi.growntilebox(1);
+        const Box gtbx = mfi.nodaltilebox(0).grow(1);
+        const Box gtby = mfi.nodaltilebox(1).grow(1);
+        const Box gtbz = mfi.nodaltilebox(2).grow(IntVect(1,1,0));
 
         amrex::ParallelFor(gbx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept {
             old_drho(i,j,k) = cur_data(i,j,k,Rho_comp) - cell_stage(i,j,k,Rho_comp);