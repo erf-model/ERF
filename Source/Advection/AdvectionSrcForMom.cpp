1d37fa73cd94fe6fea009c2b31a0c044083bafc8
Ann Almgren <asalmgren@lbl.gov>
2022-09-08 19:38:08 -0700

    speed up momentum advection

--- Source/Advection/AdvectionSrcForMom.cpp
+++ Source/Advection/AdvectionSrcForMom.cpp
@@ -20,6 +20,8 @@ AdvectionSrcForMom (int level, const Box& bx, const Box& valid_bx,
 {
     BL_PROFILE_VAR("AdvectionSrcForMom", AdvectionSrcForMom);
 
+    auto dxInv = cellSizeInv[0], dyInv = cellSizeInv[1], dzInv = cellSizeInv[2];
+
     Box bxx = surroundingNodes(bx,0);
     Box bxy = surroundingNodes(bx,1);
     Box bxz = surroundingNodes(bx,2);
@@ -61,7 +63,61 @@ AdvectionSrcForMom (int level, const Box& bx, const Box& valid_bx,
             rho_w_rhs(i, j, k) = -AdvectionSrcForZMom_T(i, j, k, rho_u, rho_v, rho_w, z_t, w, z_nd, detJ,
                                                         cellSizeInv, spatial_order, domhi_z);
         });
-    } else {
+    } else if (spatial_order == 2) {
+        amrex::ParallelFor(bxx, bxy, bxz,
+        [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+        {
+             Real xflux_hi = 0.25 * (rho_u(i, j  , k) + rho_u(i+1, j  , k)) * (u(i+1,j,k) + u(i,j,k));
+             Real xflux_lo = 0.25 * (rho_u(i, j  , k) + rho_u(i-1, j  , k)) * (u(i-1,j,k) + u(i,j,k));
+
+             Real yflux_hi = 0.25 * (rho_v(i, j+1, k) + rho_v(i-1, j+1, k)) * (u(i,j+1,k) + u(i,j,k));
+             Real yflux_lo = 0.25 * (rho_v(i, j  , k) + rho_v(i-1, j  , k)) * (u(i,j-1,k) + u(i,j,k));
+
+             Real zflux_hi = 0.25 * (rho_w(i, j, k+1) + rho_w(i-1, j, k+1)) * (u(i,j,k+1) + u(i,j,k));
+             Real zflux_lo = 0.25 * (rho_w(i, j, k  ) + rho_w(i-1, j, k  )) * (u(i,j,k-1) + u(i,j,k));
+
+             Real advectionSrc = (xflux_hi - xflux_lo) * dxInv
+                               + (yflux_hi - yflux_lo) * dyInv
+                               + (zflux_hi - zflux_lo) * dzInv;
+             rho_u_rhs(i, j, k) = -advectionSrc;
+        },
+        [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+        {
+             Real xflux_hi = 0.25 * (rho_u(i+1, j, k) + rho_u(i+1, j-1, k)) * (v(i+1,j,k) + v(i,j,k));
+             Real xflux_lo = 0.25 * (rho_u(i  , j, k) + rho_u(i  , j-1, k)) * (v(i-1,j,k) + v(i,j,k));
+
+             Real yflux_hi = 0.25 * (rho_v(i, j, k  ) + rho_v(i  , j+1, k)) * (v(i,j+1,k) + v(i,j,k));
+             Real yflux_lo = 0.25 * (rho_v(i, j, k  ) + rho_v(i  , j-1, k)) * (v(i,j-1,k) + v(i,j,k));
+
+             Real zflux_hi = 0.25 * (rho_w(i, j, k+1) + rho_w(i, j-1, k+1)) * (v(i,j,k+1) + v(i,j,k));
+             Real zflux_lo = 0.25 * (rho_w(i, j, k  ) + rho_w(i, j-1, k  )) * (v(i,j,k-1) + v(i,j,k));
+
+             Real advectionSrc = (xflux_hi - xflux_lo) * dxInv
+                               + (yflux_hi - yflux_lo) * dyInv
+                               + (zflux_hi - zflux_lo) * dzInv;
+             rho_v_rhs(i, j, k) = -advectionSrc;
+        },
+        [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+        {
+             Real xflux_hi = 0.25*(rho_u(i+1, j, k) + rho_u(i+1, j, k-1)) * (w(i+1,j,k) + w(i,j,k));
+             Real xflux_lo = 0.25*(rho_u(i  , j, k) + rho_u(i  , j, k-1)) * (w(i-1,j,k) + w(i,j,k));
+
+             Real yflux_hi = 0.25*(rho_v(i, j+1, k) + rho_v(i, j+1, k-1)) * (w(i,j+1,k) + w(i,j,k));
+             Real yflux_lo = 0.25*(rho_v(i, j  , k) + rho_v(i, j  , k-1)) * (w(i,j-1,k) + w(i,j,k));
+
+             Real zflux_lo = (k == 0) ? rho_w(i,j,k) * w(i,j,k) :
+                 0.25 * (rho_w(i,j,k) + rho_w(i,j,k-1)) * (w(i,j,k) + w(i,j,k-1));
+
+             Real zflux_hi = (k == domhi_z+1) ? rho_w(i,j,k) * w(i,j,k) :
+                 0.25 * (rho_w(i,j,k) + rho_w(i,j,k+1)) * (w(i,j,k) + w(i,j,k+1));
+
+             Real advectionSrc = (xflux_hi - xflux_lo) * dxInv
+                               + (yflux_hi - yflux_lo) * dyInv
+                               + (zflux_hi - zflux_lo) * dzInv;
+             rho_w_rhs(i, j, k) = -advectionSrc;
+        });
+
+    } else { // order > 2
         amrex::ParallelFor(bxx, bxy, bxz,
         [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
         {
