3821c3f5dce971bab35aac8f1cc240e34d962b4f
AMLattanzi <AMLattanzi@lbl.gov>
2022-09-26 13:39:15 -0700

    Submodule clean up.

--- Source/Diffusion/DiffusionSrcForState_N.cpp
+++ Source/Diffusion/DiffusionSrcForState_N.cpp
@@ -61,10 +61,12 @@ DiffusionSrcForState_N (const amrex::Box& bx, const amrex::Box& domain, int n_st
     Vector<int> eddy_diff_idz{EddyDiff::Theta_v, EddyDiff::KE_v, EddyDiff::QKE_v, EddyDiff::Scalar_v};
 
     // Device vectors
-    Gpu::DeviceVector<Real> alpha_eff_d;
-    Gpu::DeviceVector<int>  eddy_diff_idx_d,eddy_diff_idy_d,eddy_diff_idz_d;
-    alpha_eff_d.resize(4,0.);
-    eddy_diff_idx_d.resize(4,0.); eddy_diff_idy_d.resize(4,0.); eddy_diff_idz_d.resize(4,0.);
+    Gpu::AsyncVector<Real> alpha_eff_d;
+    Gpu::AsyncVector<int>  eddy_diff_idx_d,eddy_diff_idy_d,eddy_diff_idz_d;
+    alpha_eff_d.resize(alpha_eff.size());
+    eddy_diff_idx_d.resize(eddy_diff_idx.size());
+    eddy_diff_idy_d.resize(eddy_diff_idy.size());
+    eddy_diff_idz_d.resize(eddy_diff_idz.size());
 
     Gpu::copy(Gpu::hostToDevice, alpha_eff.begin(), alpha_eff.end(), alpha_eff_d.begin());
     Gpu::copy(Gpu::hostToDevice, eddy_diff_idx.begin(), eddy_diff_idx.end(), eddy_diff_idx_d.begin());
