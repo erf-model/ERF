13aecc5d10a36a0208ec7b24d677857babd263b3
Aaron Lattanzi <amlattanzi@ghidorah.dhcp.lbl.gov>
2023-02-17 13:24:26 -0800

    Fix post RHS to update RhoKE and RhoQKE.

--- Source/Diffusion/DiffusionSrcForState_N.cpp
+++ Source/Diffusion/DiffusionSrcForState_N.cpp
@@ -342,6 +342,7 @@ DiffusionSrcForState_N (const amrex::Box& bx, const amrex::Box& domain, int n_st
 
     // Using Deardorff
     if (l_use_deardorff && n_end >= RhoKE_comp) {
+      amrex::Print() << "STARTED...\n";
         int qty_index = RhoKE_comp;
         amrex::ParallelFor(bx,[=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
         {
@@ -353,7 +354,7 @@ DiffusionSrcForState_N (const amrex::Box& bx, const amrex::Box& domain, int n_st
             if (dtheta_dz <= 0.) {
                 length = l_Delta;
             } else {
-                length = 0.76*std::sqrt(E)*(grav_gpu[2]/theta)*dtheta_dz;
+              length = 0.76*std::sqrt(E) / std::sqrt((std::abs(grav_gpu[2])/theta)*dtheta_dz);
             }
             Real KH   = 0.1 * (1.+2.*length/l_Delta) * std::sqrt(E);
             cell_rhs(i,j,k,qty_index) += cell_data(i,j,k,Rho_comp) * grav_gpu[2] * KH * dtheta_dz;
