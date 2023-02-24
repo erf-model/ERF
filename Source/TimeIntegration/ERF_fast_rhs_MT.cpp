25afd903439fdbbaf69d621739313e0cf10d40d0
Ann Almgren <asalmgren@lbl.gov>
2023-02-23 18:54:15 -0800

    fixes for gbxo

--- Source/TimeIntegration/ERF_fast_rhs_MT.cpp
+++ Source/TimeIntegration/ERF_fast_rhs_MT.cpp
@@ -287,15 +287,15 @@ void erf_fast_rhs_MT (int step, int /*level*/,
         Box gbxo = mfi.nodaltilebox(2) & surroundingNodes(valid_bx,2);
         {
         BL_PROFILE("fast_MT_making_omega");
-        Box gbxo_lo = gbxo; gbxo.setBig(2,0);
+        Box gbxo_lo = gbxo; gbxo_lo.setBig(2,0);
         amrex::ParallelFor(gbxo_lo, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept {
             omega_arr(i,j,k) = 0.;
         });
-        Box gbxo_hi = gbxo; gbxo.setSmall(2,gbxo.bigEnd(2));
+        Box gbxo_hi = gbxo; gbxo_hi.setSmall(2,gbxo.bigEnd(2));
         amrex::ParallelFor(gbxo_hi, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept {
             omega_arr(i,j,k) = prev_zmom(i,j,k) - stg_zmom(i,j,k) - zp_t_arr(i,j,k);
         });
-        Box gbxo_mid = gbxo; gbxo.setSmall(2,1); gbxo.setBig(2,gbxo.bigEnd(2)-1);
+        Box gbxo_mid = gbxo; gbxo_mid.setSmall(2,1); gbxo_mid.setBig(2,gbxo.bigEnd(2)-1);
         amrex::ParallelFor(gbxo_mid, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept {
             omega_arr(i,j,k) =
                (OmegaFromW(i,j,k,prev_zmom(i,j,k),prev_xmom,prev_ymom,z_nd_old,dxInv)
