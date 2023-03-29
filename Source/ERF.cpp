8b6d79ac010782569f3e28366fb3815603d39d63
Don E. Willcox <eugene.willcox@gmail.com>
2023-03-29 12:52:19 -0700

    Some automatic clang-tidy fixes.

--- Source/ERF.cpp
+++ Source/ERF.cpp
@@ -1441,7 +1441,7 @@ ERF::MakeHorizontalAverages ()
 
 // Create horizontal average quantities for the MultiFab passed in
 // NOTE: this does not create device versions of the 1d arrays
-void
+static void
 ERF::MakeDiagnosticAverage (Vector<Real>& h_havg, MultiFab& S, int n)
 {
     // Get the number of cells in z at level 0
