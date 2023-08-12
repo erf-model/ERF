042bdfe2998ace5e3f891e9bf568965cdecc457d
Emily Bogle <85757889+eabogle@users.noreply.github.com>
2023-08-11 19:53:47 -0700

    Changing efficient scalar advection to be function based (#1205)
    * Efficient Scalar Advection / New Prob Type
    
    * Attempt at fixing tabs
    
    * Attempt to fix tabs
    
    * Fix tabs partially
    
    * Final fix of tabs hopefully
    
    * fixing tabs
    
    * fix typo
    
    * function version of efficient advection on horiz and vert
    
    * fix whitespace
    
    * fix tabs
    
    * update call AdvectionScrForScalars once
    
    * Update Advection.H
    
    ---------
    
    Co-authored-by: Emily Bogle <eabogle@login37.chn>
    Co-authored-by: Ann Almgren <asalmgren@lbl.gov>

--- Source/TimeIntegration/ERF_slow_rhs_post.cpp
+++ Source/TimeIntegration/ERF_slow_rhs_post.cpp
@@ -45,6 +45,7 @@ using namespace amrex;
  */
 
 void erf_slow_rhs_post (int /*level*/,
+                        int nrk,
                         Real dt,
                         Vector<MultiFab>& S_rhs,
                         Vector<MultiFab>& S_old,
@@ -250,12 +251,20 @@ void erf_slow_rhs_post (int /*level*/,
 
         // This is simply an advected scalar for convenience
         start_comp = RhoScalar_comp;
-          num_comp = 1;
+        num_comp = 1;
+        AdvType horiz_adv_type = solverChoice.dryscal_horiz_adv_type;
+        AdvType vert_adv_type = solverChoice.dryscal_vert_adv_type;
+
+        if (solverChoice.use_efficient_advection){
+             horiz_adv_type = EfficientAdvType(nrk,solverChoice.dryscal_horiz_adv_type);
+             vert_adv_type = EfficientAdvType(nrk,solverChoice.dryscal_vert_adv_type);
+        }
+
         AdvectionSrcForScalars(tbx, start_comp, num_comp, avg_xmom, avg_ymom, avg_zmom,
-                               cur_prim, cell_rhs, detJ_arr, dxInv, mf_m,
-                               solverChoice.dryscal_horiz_adv_type,
-                               solverChoice.dryscal_vert_adv_type,
-                               l_use_terrain);
+                              cur_prim, cell_rhs, detJ_arr, dxInv, mf_m,
+                              horiz_adv_type,
+                              vert_adv_type,
+                              l_use_terrain);
 
 #ifdef ERF_USE_MOISTURE
         start_comp = RhoQt_comp;
