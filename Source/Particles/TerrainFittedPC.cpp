ebd6f9fcf610019e43d08ee750e5ac2cce361c2f
Andrew Myers <atmyers2@gmail.com>
2023-08-24 21:39:39 -0700

    remove unused functions

--- Source/Particles/TerrainFittedPC.cpp
+++ Source/Particles/TerrainFittedPC.cpp
@@ -4,34 +4,6 @@
 
 using namespace amrex;
 
-namespace
-{
-    void get_position_unit_cell(Real* r, const IntVect& nppc, int i_part)
-    {
-        int nx = nppc[0];
-        int ny = nppc[1];
-        int nz = nppc[2];
-
-        int ix_part = i_part/(ny * nz);
-        int iy_part = (i_part % (ny * nz)) % ny;
-        int iz_part = (i_part % (ny * nz)) / ny;
-
-        r[0] = (0.5+ix_part)/nx;
-        r[1] = (0.5+iy_part)/ny;
-        r[2] = (0.5+iz_part)/nz;
-    }
-
-    void get_gaussian_random_momentum(Real* u, Real u_mean, Real u_std) {
-        Real ux_th = amrex::RandomNormal(0.0, u_std);
-        Real uy_th = amrex::RandomNormal(0.0, u_std);
-        Real uz_th = amrex::RandomNormal(0.0, u_std);
-
-        u[0] = u_mean + ux_th;
-        u[1] = u_mean + uy_th;
-        u[2] = u_mean + uz_th;
-    }
-}
-
 void
 TerrainFittedPC::
 InitParticles (const MultiFab& a_z_height)
