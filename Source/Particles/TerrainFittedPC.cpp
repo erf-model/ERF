b911bcf0f18f9520a73e7005209b4f9192a4a6bf
Andrew Myers <atmyers2@gmail.com>
2023-08-24 23:24:00 -0700

    complete using custom cell assignor in ERF

--- Source/Particles/TerrainFittedPC.cpp
+++ Source/Particles/TerrainFittedPC.cpp
@@ -18,6 +18,20 @@ InitParticles (const MultiFab& a_z_height)
     {
         const Box& tile_box  = mfi.tilebox();
         const auto& height = a_z_height[mfi];
+        const FArrayBox* height_ptr = nullptr;
+#ifdef AMREX_USE_GPU
+        std::unique_ptr<FArrayBox> hostfab;
+        if (height.arena()->isManaged() || height.arena()->isDevice()) {
+            hostfab = std::make_unique<FArrayBox>(height.box(), height.nComp(),
+                                                  The_Pinned_Arena());
+            Gpu::dtoh_memcpy_async(hostfab->dataPtr(), height.dataPtr(),
+                                   height.size()*sizeof(Real));
+            Gpu::streamSynchronize();
+            height_ptr = hostfab.get();
+        }
+#else
+        height_ptr = &height;
+#endif
         Gpu::HostVector<ParticleType> host_particles;
         for (IntVect iv = tile_box.smallEnd(); iv <= tile_box.bigEnd(); tile_box.next(iv)) {
             Real r[3] = {0.5, 0.5, 0.5};  // this means place at cell center
@@ -25,7 +39,7 @@ InitParticles (const MultiFab& a_z_height)
 
             Real x = plo[0] + (iv[0] + r[0])*dx[0];
             Real y = plo[1] + (iv[1] + r[1])*dx[1];
-            Real z = height(iv) + r[2]*(height(iv + IntVect(AMREX_D_DECL(0, 0, 1))) - height(iv));
+            Real z = (*height_ptr)(iv) + r[2]*((*height_ptr)(iv + IntVect(AMREX_D_DECL(0, 0, 1))) - (*height_ptr)(iv));
 
             ParticleType p;
             p.id()  = ParticleType::NextID();
