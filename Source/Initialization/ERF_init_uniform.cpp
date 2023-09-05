12411cdfcdb2e1aae83446f5e867190e746291d8
Eliot Quon <eliot.quon@nrel.gov>
2023-09-05 15:23:22 -0600

    Add missing source/header files

---
+++ Source/Initialization/ERF_init_uniform.cpp
@@ -0,0 +1,25 @@
+/**
+ * \file ERF_init_uniform.cpp
+ */
+
+#include <ERF.H>
+#include <prob_common.H>
+
+using namespace amrex;
+
+/**
+ * Use problem-specific reference density and temperature to set the
+ * background state to a uniform value.
+ *
+ * @param lev Integer specifying the current level
+ */
+void
+ERF::init_uniform (int lev)
+{
+    auto& lev_new = vars_new[lev];
+    for (MFIter mfi(lev_new[Vars::cons], TilingIfNotGPU()); mfi.isValid(); ++mfi) {
+        const Box &gbx = mfi.growntilebox(1);
+        const auto &cons_arr = lev_new[Vars::cons].array(mfi);
+        prob->init_state(gbx, cons_arr);
+    }
+}
