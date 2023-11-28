8a10001fc3558e6517dda7a5aca07ee67881d3c1
AMLattanzi <AMLattanzi@lbl.gov>
2023-11-28 12:13:13 -0800

    Initi1D an only access use_moist_bkgd when compiled with moisture support.

--- Source/Initialization/ERF_init1d.cpp
+++ Source/Initialization/ERF_init1d.cpp
@@ -129,12 +129,17 @@ ERF::initHSE (int lev)
     MultiFab p_hse (base_state[lev], make_alias, 1, 1); // p_0  is second component
     MultiFab pi_hse(base_state[lev], make_alias, 2, 1); // pi_0 is third  component
 
+#ifdef ERF_USE_MOISTURE
     // Initial r_hse may or may not be in HSE -- defined in prob.cpp
     if(solverChoice.use_moist_background){
         prob->erf_init_dens_hse_moist(r_hse, z_phys_nd[lev], z_phys_cc[lev], geom[lev]);
     }else{
         prob->erf_init_dens_hse(r_hse, z_phys_nd[lev], z_phys_cc[lev], geom[lev]);
     }
+#else
+    prob->erf_init_dens_hse(r_hse, z_phys_nd[lev], z_phys_cc[lev], geom[lev]);
+#endif
+
     // This integrates up through column to update p_hse, pi_hse;
     // r_hse is not const b/c FillBoundary is called at the end for r_hse and p_hse
     erf_enforce_hse(lev, r_hse, p_hse, pi_hse, z_phys_cc[lev], z_phys_nd[lev]);
