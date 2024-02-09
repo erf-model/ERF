70dadec2bd54459ea30e3a4c9e09d3ca40a92b23
Mukul Dave <mhdave@lbl.gov>
2024-02-09 11:21:44 -0800

    remove plot/checkfile writing at the end of Evolve_MB that creates extraneous files

--- Source/ERF.cpp
+++ Source/ERF.cpp
@@ -1734,27 +1734,5 @@ ERF::Evolve_MB (int MBstep, int max_block_step)
         if (cur_time >= stop_time - 1.e-6*dt[0]) break;
     }
 
-/*  I don't think we need these for the multiblock Evolve
-**  Results in extraneous plot/checkpoint files
-
-    if (plot_int_1 > 0 && istep[0] > last_plot_file_step_1) {
-        WritePlotFile(1,plot_var_names_1);
-    }
-    if (plot_int_2 > 0 && istep[0] > last_plot_file_step_2) {
-        WritePlotFile(2,plot_var_names_2);
-    }
-
-    if (check_int > 0 && istep[0] > last_check_file_step) {
-#ifdef ERF_USE_NETCDF
-        if (check_type == "netcdf") {
-           WriteNCCheckpointFile();
-        }
-#endif
-        if (check_type == "native") {
-           WriteCheckpointFile();
-        }
-    }
-*/
-
 }
 #endif
