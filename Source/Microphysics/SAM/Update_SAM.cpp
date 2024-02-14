8e65e97d63ab32447422be4c25291f0009c192c5
AMLattanzi <AMLattanzi@lbl.gov>
2024-02-14 13:50:11 -0800

    Microphysics has enum issues.

--- Source/Microphysics/SAM/Update_SAM.cpp
+++ Source/Microphysics/SAM/Update_SAM.cpp
@@ -18,11 +18,11 @@ void SAM::Copy_Micro_to_State (amrex::MultiFab& cons)
 
         auto states_arr = cons.array(mfi);
 
-        auto rho_arr    = mic_fab_vars[MicVar_Kess::rho]->array(mfi);
-        auto theta_arr  = mic_fab_vars[MicVar_Kess::theta]->array(mfi);
-        auto qv_arr     = mic_fab_vars[MicVar_Kess::qv]->array(mfi);
-        auto qc_arr     = mic_fab_vars[MicVar_Kess::qcl]->array(mfi);
-        auto qp_arr     = mic_fab_vars[MicVar_Kess::qp]->array(mfi);
+        auto rho_arr    = mic_fab_vars[MicVar::rho]->array(mfi);
+        auto theta_arr  = mic_fab_vars[MicVar::theta]->array(mfi);
+        auto qv_arr     = mic_fab_vars[MicVar::qv]->array(mfi);
+        auto qc_arr     = mic_fab_vars[MicVar::qcl]->array(mfi);
+        auto qp_arr     = mic_fab_vars[MicVar::qp]->array(mfi);
 
         // get potential total density, temperature, qt, qp
         ParallelFor( box3d, [=] AMREX_GPU_DEVICE (int i, int j, int k)
