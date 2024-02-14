79972f9bf406b12ee24117f48fba7f25d4b10486
Debojyoti Ghosh <debojyoti.ghosh@gmail.com>
2024-02-13 17:19:15 -0800

    adding functions to compute cloud density and mixing ratio; added total mixing ratio as a microphysics variable since ERF needs it; microphysics vars are mixing ratios since that's the convention; added some util functions

--- Source/Microphysics/SuperDropletsMoist/SuperDropletsMoistInit.cpp
+++ Source/Microphysics/SuperDropletsMoist/SuperDropletsMoistInit.cpp
@@ -31,9 +31,9 @@ void SuperDropletsMoist::Init ( const MultiFab&   a_cons_vars,  /*!< Conserved v
     m_dt = a_dt;
 
     m_mic_var_map.resize(m_qmoist_size);
-    m_mic_var_map = {MicVar_SD::rho_v, MicVar_SD::rho_c};
+    m_mic_var_map = {MicVar_SD::q_t, MicVar_SD::q_v, MicVar_SD::q_c};
 
-    /* allocate vapour and condensate multifabs */
+    /* allocate microphysics multifabs */
     for (auto i(0); i < MicVar_SD::NumVars; i++) {
       m_mic_fab_vars[i] = std::make_shared<MultiFab> ( a_cons_vars.boxArray(),
                                                        a_cons_vars.DistributionMap(),
@@ -67,18 +67,39 @@ void SuperDropletsMoist::Init ( const MultiFab&   a_cons_vars,  /*!< Conserved v
 }
 
 /*! Finish any initializations that depend on the conserved state variables
-    that were not available during Init() */
-void SuperDropletsMoist::FinishInit ()
+    that were not available during Init(); also, overwrite the rhoq2 component
+    with the quantity computed from the this model. */
+void SuperDropletsMoist::FinishInit (MultiFab& a_cons_vars /*!< Conserved variables */)
 {
     if (m_init_type == SuperDropletsMoistInitializations::init_rhoc) {
         /* initial super-droplets attributes computed from condensate mass density */
-        m_super_droplets->SetAttributes(*(m_mic_fab_vars[MicVar_SD::rho_c]));
+        MultiFab rho_c ( m_mic_fab_vars[MicVar_SD::q_c]->boxArray(),
+                         m_mic_fab_vars[MicVar_SD::q_c]->DistributionMap(),
+                         1,
+                         m_mic_fab_vars[MicVar_SD::q_c]->nGrowVect() );
+        MultiFab::Copy( rho_c, *m_mic_fab_vars[MicVar_SD::q_c], 0, 0, 1, rho_c.nGrowVect() );
+        ratioToDensity(rho_c);
+
+        m_super_droplets->SetAttributes(rho_c);
         amrex::Print() << "Initialized "
                        << m_super_droplets->NumSuperDroplets()
                        << " super-droplets representing "
                        << m_super_droplets->TotalNumberOfParticles()
                        << " particles in super-droplets moisture model.\n";
     }
+
+    computeQc();
+    for ( MFIter mfi(a_cons_vars); mfi.isValid(); ++mfi) {
+        const auto& box = mfi.tilebox();
+        auto states_arr = a_cons_vars.array(mfi);
+        auto rho_arr = m_mic_fab_vars[MicVar_SD::rho]->array(mfi);
+        auto q_c_arr = m_mic_fab_vars[MicVar_SD::q_c]->array(mfi);
+        ParallelFor( box, [=] AMREX_GPU_DEVICE (int i, int j, int k)
+        {
+            states_arr(i,j,k,RhoQ2_comp) = states_arr(i,j,k,Rho_comp)*q_c_arr(i,j,k);
+        });
+    }
+
     return;
 }
 
--- Source/Microphysics/SuperDropletsMoist/SuperDropletsMoistUtils.cpp
+++ Source/Microphysics/SuperDropletsMoist/SuperDropletsMoistUtils.cpp
@@ -10,13 +10,17 @@ void SuperDropletsMoist::Copy_State_to_Micro (  const MultiFab& a_cons_vars /*!<
         const auto& box = mfi.tilebox();
         auto states_arr = a_cons_vars.array(mfi);
 
-        auto rho_v_arr = m_mic_fab_vars[MicVar_SD::rho_v]->array(mfi);
-        auto rho_c_arr = m_mic_fab_vars[MicVar_SD::rho_c]->array(mfi);
+        auto rho_arr = m_mic_fab_vars[MicVar_SD::rho]->array(mfi);
+        auto q_t_arr = m_mic_fab_vars[MicVar_SD::q_t]->array(mfi);
+        auto q_v_arr = m_mic_fab_vars[MicVar_SD::q_v]->array(mfi);
+        auto q_c_arr = m_mic_fab_vars[MicVar_SD::q_c]->array(mfi);
 
         ParallelFor( box, [=] AMREX_GPU_DEVICE (int i, int j, int k)
         {
-            rho_v_arr(i,j,k) = states_arr(i,j,k,RhoQ1_comp);
-            rho_c_arr(i,j,k) = states_arr(i,j,k,RhoQ2_comp);
+            rho_arr(i,j,k) = states_arr(i,j,k,Rho_comp);
+            q_v_arr(i,j,k) = states_arr(i,j,k,RhoQ1_comp) / states_arr(i,j,k,Rho_comp);
+            q_c_arr(i,j,k) = states_arr(i,j,k,RhoQ2_comp) / states_arr(i,j,k,Rho_comp);
+            q_t_arr(i,j,k) = q_v_arr(i,j,k) + q_c_arr(i,j,k);
         });
     }
 }
@@ -29,13 +33,47 @@ void SuperDropletsMoist::Copy_Micro_to_State (  MultiFab& a_cons_vars /*!< Conse
         const auto& box = mfi.tilebox();
         auto states_arr = a_cons_vars.array(mfi);
 
-        auto rho_v_arr = m_mic_fab_vars[MicVar_SD::rho_v]->array(mfi);
-        auto rho_c_arr = m_mic_fab_vars[MicVar_SD::rho_c]->array(mfi);
+        auto q_v_arr = m_mic_fab_vars[MicVar_SD::q_v]->array(mfi);
+        auto q_c_arr = m_mic_fab_vars[MicVar_SD::q_c]->array(mfi);
 
         ParallelFor( box, [=] AMREX_GPU_DEVICE (int i, int j, int k)
         {
-            states_arr(i,j,k,RhoQ1_comp) = rho_v_arr(i,j,k);
-            states_arr(i,j,k,RhoQ2_comp) = rho_c_arr(i,j,k);
+            states_arr(i,j,k,RhoQ1_comp) = states_arr(i,j,k,Rho_comp)*q_v_arr(i,j,k);
+            states_arr(i,j,k,RhoQ2_comp) = states_arr(i,j,k,Rho_comp)*q_c_arr(i,j,k);
+        });
+    }
+}
+
+/*! Convert a multifab containing density of something to its mixing ratio */
+void SuperDropletsMoist::densityToRatio (  MultiFab& a_var, /*!< Multifab */
+                                           const int a_comp /*!< Component */ )
+{
+    for ( MFIter mfi(a_var); mfi.isValid(); ++mfi) {
+        const auto& box = mfi.tilebox();
+
+        auto rho_arr = m_mic_fab_vars[MicVar_SD::rho]->array(mfi);
+        auto fab_arr = a_var.array(mfi);
+
+        ParallelFor( box, [=] AMREX_GPU_DEVICE (int i, int j, int k)
+        {
+            fab_arr(i,j,k,a_comp) /= rho_arr(i,j,k);
+        });
+    }
+}
+
+/*! Convert a multifab containing the mixing ratio of something to its density */
+void SuperDropletsMoist::ratioToDensity (  MultiFab& a_var, /*!< Multifab */
+                                           const int a_comp /*!< Component */ )
+{
+    for ( MFIter mfi(a_var); mfi.isValid(); ++mfi) {
+        const auto& box = mfi.tilebox();
+
+        auto rho_arr = m_mic_fab_vars[MicVar_SD::rho]->array(mfi);
+        auto fab_arr = a_var.array(mfi);
+
+        ParallelFor( box, [=] AMREX_GPU_DEVICE (int i, int j, int k)
+        {
+            fab_arr(i,j,k,a_comp) *= rho_arr(i,j,k);
         });
     }
 }
