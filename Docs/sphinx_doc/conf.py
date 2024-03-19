ea9dc6a19fd8d117f2b42c2defa5652ce33dd1f1
Bruce Perry <bruce.perry@nrel.gov>
2024-03-19 17:53:19 -0600

    error on warnings in linkcheck but ignore DOI redirects

--- Docs/sphinx_doc/conf.py
+++ Docs/sphinx_doc/conf.py
@@ -163,7 +163,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
@@ -331,6 +331,12 @@ linkcheck_ignore = [
     'https://doi.org/10.1029/RG020i004p00851',
     'https://doi.org/10.1002/qj.665'
 ]
+
+linkcheck_allowed_redirects = {
+    r'https://doi\.org/*' : '.*',
+    r'https://dx\.doi\.org/*' : '.*',
+    r'http://dx\.doi\.org/*' : '.*'
+}
 
 # -- Options for Texinfo output -------------------------------------------
 
