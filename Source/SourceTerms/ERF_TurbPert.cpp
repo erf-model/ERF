fd35ec2a7a8e56263c05d8f9673df23d96bb17e0
Ting-Hsuan (Dustin) Ma <tma@megalon.dhcp.lbl.gov>
2024-05-20 11:09:19 -0700

    Box perturbation setup start

---
+++ Source/SourceTerms/ERF_TurbPert.cpp
@@ -0,0 +1,36 @@
+#include <AMReX_MultiFab.H>
+#include <Src_headers.H>
+
+using namespace amrex;
+
+void
+CalcTurbPert (
+  const Box& bx,
+  const Array4<Real>& cell_rhs,
+  const Array4<const Real>& cell_data)
+{
+    // Domain cell size and real bounds
+    // auto dx = geom.CellSizeArray();
+    // auto ProbHiArr = geom.ProbHiArray();
+    // auto ProbLoArr = geom.ProbLoArray();
+
+    int n = RhoTheta_comp;  
+
+    Box mybox(IntVect(1,0,0), IntVect(3,2,2));
+
+    if (bx.contains(mybox)) {
+
+    ParallelFor(mybox, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept
+    {
+        cell_rhs(i, j, k, n) += 0.0;
+    });
+    } else {
+        amrex::Abort("mybox not in bx!");
+    }
+}
+
+
+
+
+
+
