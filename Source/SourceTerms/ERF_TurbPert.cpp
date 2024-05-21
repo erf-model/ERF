2c71db3263b66ba820ba5c20e3946591dd67edcc
Ting-Hsuan (Dustin) Ma <tma@megalon.dhcp.lbl.gov>
2024-05-21 15:59:35 -0700

    Using index of box to created boxes

--- Source/SourceTerms/ERF_TurbPert.cpp
+++ Source/SourceTerms/ERF_TurbPert.cpp
@@ -9,23 +9,42 @@ void CalcTurbPert (
   const Array4<Real>& cell_rhs,
   const Array4<const Real>& cell_data)
 {
+    Print() << "Passed in Box: " << bx << "\n";
+
     // Domain cell size and real bounds
     auto dx = geom.CellSizeArray();
     auto ProbHiArr = geom.ProbHiArray();
     auto ProbLoArr = geom.ProbLoArray();
 
-    int n = RhoTheta_comp;  
+    // Box creation through index values (for now)
+    Box pertRegionBox(IntVect(0,0,0), IntVect(15,31,31));
+    BoxArray pertRegionBoxArray(pertRegionBox);  // creating a box array of this region
+    pertRegionBoxArray.maxSize(16);
+    Print() << "Dividing created box into box array of element: "  << pertRegionBoxArray.size() << "\n";
 
-    Box mybox(IntVect(1,0,0), IntVect(3,2,2));
-
-    if (bx.contains(mybox)) {
-
-    ParallelFor(mybox, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept
+    // 1D array incremental values
+    Real max_i = bx.size()[0];
+    Real max_j = bx.size()[1];
+    for (int boxIdx = 0; boxIdx < pertRegionBoxArray.size(); boxIdx++)
     {
-        cell_rhs(i, j, k, n) += 0.0;
-    });
-    } else {
-        amrex::Abort("mybox not in bx!");
+        if (bx.contains(pertRegionBoxArray[boxIdx]))
+        //if (bx.contains(pertRegionBox))
+        {
+            Print() << "My box: " << bx << " Contains sub box" << boxIdx << ": " << pertRegionBoxArray[boxIdx] << "\n";
+            ParallelFor(pertRegionBox, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept
+            {
+                // Creating incremental value pertRegionBoxArraysed on indexing
+                //Real tmp_Src = (i + j*max_i + k*max_i*max_j);
+                Real tmp_Src = (Real) boxIdx;
+     
+                // Adding temperature source onto RHS
+                cell_rhs(i, j, k, RhoTheta_comp) = tmp_Src;
+            });
+        // Took these two lines out because it's okay if the region isn't in the right box
+        // Depending on the if statement to do it's job 
+        //} else {
+        //    amrex::Abort("pertRegionBox not in bx!");
+        }
     }
 }
 
