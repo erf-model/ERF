aadd48015ac07a398e5552e88315d5fc67dc6e72
Ting-Hsuan (Dustin) Ma <tma@megalon.dhcp.lbl.gov>
2024-05-22 09:57:53 -0700

    Index Perturbation box initialization working

--- Source/SourceTerms/ERF_TurbPert.cpp
+++ Source/SourceTerms/ERF_TurbPert.cpp
@@ -3,53 +3,47 @@
 
 using namespace amrex;
 
+//#define DEBUG_PERT_MSG
+
 void CalcTurbPert (
   const Geometry geom,
   const Box& bx,
   const Array4<Real>& cell_rhs,
   const Array4<const Real>& cell_data)
 {
-    Print() << "Passed in Box: " << bx << "\n";
-
     // Domain cell size and real bounds
-    auto dx = geom.CellSizeArray();
-    auto ProbHiArr = geom.ProbHiArray();
-    auto ProbLoArr = geom.ProbLoArray();
+    //auto dx = geom.CellSizeArray();
+    //auto ProbHiArr = geom.ProbHiArray();
+    //auto ProbLoArr = geom.ProbLoArray();
 
     // Box creation through index values (for now)
-    Box pertRegionBox(IntVect(0,0,0), IntVect(15,31,31));
-    BoxArray pertRegionBoxArray(pertRegionBox);  // creating a box array of this region
-    pertRegionBoxArray.maxSize(16);
-    Print() << "Dividing created box into box array of element: "  << pertRegionBoxArray.size() << "\n";
+    int offset_pertBox = 0;
+    Box pertRegionBox(IntVect(0+offset_pertBox,0,0), IntVect(7+offset_pertBox,31,31)); // for visualization purpose
+    BoxArray pertRegionBoxArray(pertRegionBox);                                        // creating a box array of this region
+    pertRegionBoxArray.maxSize(8);                                                     // Further dividing box into box array
 
-    // 1D array incremental values
-    Real max_i = bx.size()[0];
-    Real max_j = bx.size()[1];
-    for (int boxIdx = 0; boxIdx < pertRegionBoxArray.size(); boxIdx++)
+    #if DEBUG_PERT_MSG
+    Print() << "Dividing created box into box array of element: "  << pertRegionBoxArray.size() << "\n";
+    #endif
+
+    for (int boxIdx = 0; boxIdx < pertRegionBoxArray.size(); boxIdx++) // Is this the best way to iteratre through the box array?
     {
         if (bx.contains(pertRegionBoxArray[boxIdx]))
-        //if (bx.contains(pertRegionBox))
         {
-            Print() << "My box: " << bx << " Contains sub box" << boxIdx << ": " << pertRegionBoxArray[boxIdx] << "\n";
-            ParallelFor(pertRegionBox, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept
+            #if DEBUG_PERT_MSG
+            Print() << "bx: " << bx << " -- contains perturbation box #" << boxIdx << ": " << pertRegionBoxArray[boxIdx] << "\n";
+            #endif
+
+            // Parallel iterate through the box array and assign values
+            ParallelFor(pertRegionBoxArray[boxIdx], [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept
             {
-                // Creating incremental value pertRegionBoxArraysed on indexing
-                //Real tmp_Src = (i + j*max_i + k*max_i*max_j);
-                Real tmp_Src = (Real) boxIdx;
+                // Creating artificial values to populate the boxes
+                //Real tmp_Src = (i + j*bx.size()[0] + k*bx.size()[0]*bx.size()[1]); // continuous count
+                Real tmp_Src = (Real) boxIdx*1e5; // distinguish each box
      
                 // Adding temperature source onto RHS
                 cell_rhs(i, j, k, RhoTheta_comp) = tmp_Src;
             });
-        // Took these two lines out because it's okay if the region isn't in the right box
-        // Depending on the if statement to do it's job 
-        //} else {
-        //    amrex::Abort("pertRegionBox not in bx!");
         }
     }
 }
-
-
-
-
-
-
