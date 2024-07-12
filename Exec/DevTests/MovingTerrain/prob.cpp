8f84adaeaba5c330419f5d32fe516b8dc7724aa4
Bruce Perry <bruce.perry@nrel.gov>
2024-07-12 17:12:50 -0600

    fix one more warning

--- Exec/DevTests/MovingTerrain/prob.cpp
+++ Exec/DevTests/MovingTerrain/prob.cpp
@@ -45,9 +45,6 @@ Problem::init_custom_pert(
 {
   const bool use_moisture = (sc.moisture_type != MoistureType::None);
 
-  const Real T_sfc    = parms.T_0;
-  //const Real thetabar = T_sfc;
-
   Real H           = geomdata.ProbHi()[2];
   Real Ampl        = parms.Ampl;
   Real wavelength  = 100.;
