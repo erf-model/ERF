e91cd2b1cc34cc7e8036280f6a4775af4a6c685f
Ting-Hsuan (Dustin) Ma <tma@megalon.dhcp.lbl.gov>
2024-07-16 14:17:57 -0700

    fix to input tag

---
+++ Exec/DevTests/ABL_perturbation_inflow/generateLogLawProfile.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+# Given parameters
+Re_tau = 2000
+kinematic_viscosity = 0.0001
+height_start = 1e-2
+height_end = 8.0 # Channel half height
+num_points = 256
+kappa = 0.41  # Von Kármán constant
+B = 5.2
+
+# Calculate friction velocity (u_tau) based on Re_tau, kinematic viscosity, and height_end
+u_tau = (Re_tau * kinematic_viscosity) / height_end
+print(f"u_tau = {u_tau}")
+
+# Generate heights (y)
+heights = np.linspace(height_start, height_end, num_points)
+
+# Calculate u values using smooth wall log law of the wall formula
+u = u_tau * (1. / kappa * np.log(heights * u_tau / kinematic_viscosity) + B)
+print(f"u = {u}")
+
+# Create v values (set all v values to 0)
+v = np.zeros_like(heights)
+mixing_ratio = np.zeros_like(heights)
+potential_temp = np.full_like(heights, 300.0)
+
+# Prepare data to write to file
+data_sounding = np.column_stack((heights, potential_temp, mixing_ratio, u, v))
+data_sponge = np.column_stack((heights, u, v))
+
+# Add the initial rows
+data_sounding = np.vstack([[0.0, 300.0, 0.0, 0.0, 0.0], data_sounding])
+data_sponge = np.vstack([[0.0, 0.0, 0.0], data_sponge])
+
+# Save data to file
+filename = "input_ReTau2000Ana"
+filetype = ".txt"
+
+np.savetxt(filename + "_sounding" + filetype, data_sounding, fmt="%.8e", delimiter=' ')
+print(f"Data saved to {filename}_sounding{filetype}.")
+np.savetxt(filename + "_sponge" + filetype, data_sponge, fmt="%.8e", delimiter=' ')
+print(f"Data saved to {filename}_sponge{filetype}.")
