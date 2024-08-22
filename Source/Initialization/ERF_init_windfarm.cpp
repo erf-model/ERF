4b4b2c72ac713b1cf740ec2bf3aea54420ec6ef4
Mahesh Natarajan <nataraj2@login18.chn.perlmutter.nersc.gov>
2024-08-22 14:59:53 -0700

    Fill SMark only for actuator disks

--- Source/Initialization/ERF_init_windfarm.cpp
+++ Source/Initialization/ERF_init_windfarm.cpp
@@ -33,10 +33,10 @@ ERF::init_windfarm (int lev)
     windfarm->write_turbine_locations_vtk();
 
     if(solverChoice.windfarm_type == WindFarmType::SimpleAD) {
+        windfarm->fill_SMark_multifab(geom[lev], SMark[lev], solverChoice.sampling_distance_by_D);
         windfarm->write_actuator_disks_vtk(geom[lev]);
     }
 
-    windfarm->fill_SMark_multifab(geom[lev], SMark[lev], solverChoice.sampling_distance_by_D);
 }
 
 void
