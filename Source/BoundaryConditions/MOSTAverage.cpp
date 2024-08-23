b4574fe8ebcea817598072b853750078297f2ff4
Eliot Quon <eliot.quon@nrel.gov>
2024-08-23 11:39:25 -0600

    Implement smarter Vsg fix

--- Source/BoundaryConditions/MOSTAverage.cpp
+++ Source/BoundaryConditions/MOSTAverage.cpp
@@ -203,21 +203,19 @@ MOSTAverage::MOSTAverage (Vector<Geometry>  geom,
     // Corrections to the mean surface velocity
     pp.query("most.wstar_beta", m_wstar_beta);
     pp.query("most.include_subgrid_vel", include_subgrid_vel);
-    Vector<Real> Vsg(m_maxlev, 0.0);
+    m_Vsg = Vector<Real>(m_maxlev, 0.0);
     if (include_subgrid_vel) {
         amrex::Print() << "Subgrid velocity scale correction (by level) : ";
         for (int lev(0); lev < m_maxlev; lev++) {
             const auto dxArr = m_geom[lev].CellSizeArray();
             Real dx = std::sqrt(dxArr[0]*dxArr[1]);
             if (dx > 5000.) {
-                Vsg[lev] = 0.32 * std::pow(dx/5000.-1, 0.33);
+                m_Vsg[lev] = 0.32 * std::pow(dx/5000.-1, 0.33);
             }
-            amrex::Print() << Vsg[lev];
+            amrex::Print() << m_Vsg[lev];
         }
         amrex::Print() << std::endl;
     }
-    d_Vsg.resize(m_maxlev);
-    Gpu::copy(Gpu::hostToDevice, Vsg.begin(), Vsg.end(), d_Vsg.begin());
 }
 
 
@@ -896,6 +894,7 @@ MOSTAverage::compute_plane_averages (int lev)
         val_old[iavg] = plane_average[iavg]*d_fact_old;
 
         const Real beta = m_wstar_beta;
+        const Real Vsg = m_Vsg[lev];
 
 #ifdef _OPENMP
 #pragma omp parallel if (Gpu::notInLaunchRegion())
@@ -931,7 +930,7 @@ MOSTAverage::compute_plane_averages (int lev)
                                             &v_interp, v_mf_arr, z_phys_arr, plo, dxInv, 1);
                     const Real wst = (wstar_arr) ? beta*wstar_arr(i,j,k) : 0.0;
                     const Real val = std::sqrt(u_interp*u_interp + v_interp*v_interp
-                                               + wst*wst + d_Vsg[lev]*d_Vsg[lev]);
+                                               + wst*wst + Vsg*Vsg);
                     Gpu::deviceReduceSum(&plane_avg[iavg], val, handler);
                 });
             } else {
@@ -948,7 +947,7 @@ MOSTAverage::compute_plane_averages (int lev)
                     const Real v_val = 0.5 * (v_mf_arr(mi,mj,mk) + v_mf_arr(mi  ,mj+1,mk));
                     const Real wst = (wstar_arr) ? beta*wstar_arr(i,j,k) : 0.0;
                     const Real val = std::sqrt(u_val*u_val + v_val*v_val
-                                               + wst*wst + d_Vsg[lev]*d_Vsg[lev]);
+                                               + wst*wst + Vsg*Vsg);
                     Gpu::deviceReduceSum(&plane_avg[iavg], val, handler);
                 });
             }
@@ -1194,6 +1193,7 @@ MOSTAverage::compute_region_averages (int lev)
         int iavg = m_navg - 1;
 
         const Real beta = m_wstar_beta;
+        const Real Vsg = m_Vsg[lev];
 
 #ifdef _OPENMP
 #pragma omp parallel if (Gpu::notInLaunchRegion())
@@ -1234,7 +1234,7 @@ MOSTAverage::compute_region_averages (int lev)
                             trilinear_interp_T(xp, yp, zp, &v_interp, v_mf_arr, z_phys_arr, plo, dxInv, 1);
                             const Real wst = (wstar_arr) ? beta*wstar_arr(i,j,k) : 0.0;
                             const Real mag = std::sqrt(u_interp*u_interp + v_interp*v_interp
-                                                       + wst*wst + d_Vsg[lev]*d_Vsg[lev]);
+                                                       + wst*wst + Vsg*Vsg);
                             Real val = denom * mag * d_fact_new;
                             ma_arr(i,j,k) += val;
                         }
@@ -1259,7 +1259,7 @@ MOSTAverage::compute_region_averages (int lev)
                             const Real v_val = 0.5 * (v_mf_arr(li,lj,lk) + v_mf_arr(li  ,lj+1,lk));
                             const Real wst = (wstar_arr) ? beta*wstar_arr(i,j,k) : 0.0;
                             const Real mag   = std::sqrt(u_val*u_val + v_val*v_val
-                                                         + wst*wst + d_Vsg[lev]*d_Vsg[lev]);
+                                                         + wst*wst + Vsg*Vsg);
                             Real val = denom * mag * d_fact_new;
                             ma_arr(i,j,k) += val;
                         }
