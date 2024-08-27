545d497dc40512bfce35f34dde9889e6ea577e47
Eliot Quon <eliot.quon@nrel.gov>
2024-08-27 14:31:10 -0600

    Fix restart with input sounding data

--- Source/Initialization/ERF_init1d.cpp
+++ Source/Initialization/ERF_init1d.cpp
@@ -61,7 +61,10 @@ ERF::setRayleighRefFromSounding (bool restarting)
     //    so we need to read it here
     // TODO: should we store this information in the checkpoint file instead?
     if (restarting) {
-        input_sounding_data.read_from_file(geom[0], zlevels_stag, 0);
+        input_sounding_data.resize_arrays();
+        for (int n = 0; n < input_sounding_data.n_sounding_files; n++) {
+            input_sounding_data.read_from_file(geom[0], zlevels_stag, n);
+        }
     }
 
     const Real* z_inp_sound     = input_sounding_data.z_inp_sound[0].dataPtr();
