2ed392f2bfaa098e07216c8270526f7ba507bf61
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2024-08-28 23:33:31 -0500

    Initial interface for NOAH LSM

--- CMake/BuildERFExe.cmake
+++ CMake/BuildERFExe.cmake
@@ -75,6 +75,13 @@ function(build_erf_lib erf_lib_name)
     target_compile_definitions(${erf_lib_name} PUBLIC ERF_USE_NETCDF)
   endif()
 
+  if(ERF_ENABLE_NOAH)
+    target_include_directories(${erf_lib_name} PUBLIC ${SRC_DIR}/LandSurfaceModel/NOAH)
+    target_sources(${erf_lib_name} PRIVATE
+                   ${SRC_DIR}/LandSurfaceModel/NOAH/NOAH.cpp)
+    target_compile_definitions(${erf_lib_name} PUBLIC ERF_USE_NOAH)
+  endif()
+
   if(ERF_ENABLE_RRTMGP)
     target_sources(${erf_lib_name} PRIVATE
                    ${SRC_DIR}/Radiation/Init_rrtmgp.cpp
---
+++ Source/LandSurfaceModel/NOAH/NOAH.cpp
@@ -0,0 +1,10 @@
+#include <NOAH.H>
+
+using namespace amrex;
+
+/* Initialize lsm data structures */
+void
+NOAH::Init (const MultiFab& cons_in,
+           const Geometry& geom,
+           const Real& dt)
+{};
