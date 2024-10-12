0a2e9b1013ed6d4094d00030b1f241283b32c2c7
Mukul Dave <mhdave@lbl.gov>
2024-10-11 17:00:13 -0700

    remove multiblock container and evolve stuff

--- Source/ERF.cpp
+++ Source/ERF.cpp
@@ -14,14 +14,6 @@
 #include <ERF_TerrainMetrics.H>
 #include <memory>
 
-#ifdef ERF_USE_MULTIBLOCK
-#ifndef ERF_MB_EXTERN       // enter only if multiblock does not involve an external class
-#include <ERF_MultiBlockContainer.H>
-#else
-#include <MultiBlockContainer.H>
-#endif
-#endif
-
 using namespace amrex;
 
 Real ERF::startCPUTime        = 0.0;
@@ -568,17 +560,6 @@ ERF::InitData ()
 {
     BL_PROFILE_VAR("ERF::InitData()", InitData);
     InitData_pre();
-#if 0
-#ifdef ERF_USE_MULTIBLOCK
-#ifndef ERF_MB_EXTERN       // enter only if multiblock does not involve an external class
-        // Multiblock: hook to set BL & comms once ba/dm are known
-        if(domain_p[0].bigEnd(0) < 500 ) {
-            m_mbc->SetBoxLists();
-            m_mbc->SetBlockCommMetaData();
-        }
-#endif
-#endif
-#endif
     InitData_post();
     BL_PROFILE_VAR_STOP(InitData);
 }
@@ -2007,86 +1988,6 @@ ERF::ERF (const RealBox& rb, int max_level_in,
     domain_p.push_back(nbx);
 
     ERF_shared();
-}
-#endif
-
-#ifdef ERF_USE_MULTIBLOCK
-// advance solution over specified block steps
-void
-ERF::Evolve_MB (int MBstep, int max_block_step)
-{
-    Real cur_time = t_new[0];
-
-    int step;
-
-    // Take one coarse timestep by calling timeStep -- which recursively calls timeStep
-    // for finer levels (with or without subcycling)
-    for (int Bstep(0); Bstep < max_block_step && cur_time < stop_time; ++Bstep)
-    {
-        step = Bstep + MBstep - 1;
-
-        Print() << "\nCoarse STEP " << step+1 << " starts ..." << std::endl;
-
-        ComputeDt(step);
-
-        // Make sure we have read enough of the boundary plane data to make it through this timestep
-        if (input_bndry_planes)
-        {
-            m_r2d->read_input_files(cur_time,dt[0],m_bc_extdir_vals);
-        }
-
-        int lev = 0;
-        int iteration = 1;
-        timeStep(lev, cur_time, iteration);
-
-#ifndef ERF_MB_EXTERN
-        // DEBUG
-        // Multiblock: hook for erf2 to fill from erf1
-        if(domain_p[0].bigEnd(0) < 500) {
-            for (int var_idx = 0; var_idx < Vars::NumTypes; ++var_idx)
-                m_mbc->FillPatchBlocks(var_idx,var_idx);
-        }
-#endif
-
-        cur_time  += dt[0];
-
-        Print() << "Coarse STEP " << step+1 << " ends." << " TIME = " << cur_time
-                       << " DT = " << dt[0]  << std::endl;
-
-        post_timestep(step, cur_time, dt[0]);
-
-        if (writeNow(cur_time, dt[0], step+1, m_plot_int_1, m_plot_per_1)) {
-            last_plot_file_step_1 = step+1;
-            WritePlotFile(1,plot_var_names_1);
-        }
-
-        if (writeNow(cur_time, dt[0], step+1, m_plot_int_2, m_plot_per_2)) {
-            last_plot_file_step_2 = step+1;
-            WritePlotFile(2,plot_var_names_2);
-        }
-
-        if (writeNow(cur_time, dt[0], step+1, m_check_int, m_check_per)) {
-            last_check_file_step = step+1;
-#ifdef ERF_USE_NETCDF
-            if (check_type == "netcdf") {
-               WriteNCCheckpointFile();
-            }
-#endif
-            if (check_type == "native") {
-               WriteCheckpointFile();
-            }
-        }
-
-#ifdef AMREX_MEM_PROFILING
-        {
-            std::ostringstream ss;
-            ss << "[STEP " << step+1 << "]";
-            MemProfiler::report(ss.str());
-        }
-#endif
-
-        if (cur_time >= stop_time - 1.e-6*dt[0]) break;
-    }
 }
 #endif
 
