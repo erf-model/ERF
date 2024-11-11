a97b45671235e3f42f89b0649e03f538d669247c
Emily Katherine de Jong <dejong5@dane3.llnl.gov>
2024-11-11 15:28:14 -0800

    sample in radius, not mass

--- Source/Particles/ERF_SDInitialization.cpp
+++ Source/Particles/ERF_SDInitialization.cpp
@@ -204,8 +204,8 @@ void SDInitialization::getAerosolDistribution ( amrex::Vector<amrex::Real>& a_ae
         std::uniform_real_distribution<> urd(0.0, 1.0);
         auto sigma = m_radius_aerosol_std[a_idx];
         auto mu = m_radius_aerosol_mean[a_idx];
-        auto lnrng = std::log(m_mass_aerosol_max[a_idx] - m_mass_aerosol_min[a_idx]);
-        auto lnmin = std::log(m_mass_aerosol_min[a_idx]);
+        auto lnrng = std::log(m_radius_aerosol_max[a_idx] - m_radius_aerosol_min[a_idx]);
+        auto lnmin = std::log(m_radius_aerosol_min[a_idx]);
         for (int n = 0; n < a_np; n++) {
             auto tmp = lnmin + urd(rng) * lnrng;
             auto dry_r = std::exp(tmp);
