24e87c904330b51723b5652b1cd09e1e6a2e4b09
Debojyoti Ghosh <debojyoti.ghosh@gmail.com>
2024-12-16 13:15:50 -0800

    added BC info as an argument to microphysics advance function

--- Source/TimeIntegration/ERF_AdvanceMicrophysics.cpp
+++ Source/TimeIntegration/ERF_AdvanceMicrophysics.cpp
@@ -10,7 +10,7 @@ void ERF::advance_microphysics (int lev,
 {
     if (solverChoice.moisture_type != MoistureType::None) {
         micro->Update_Micro_Vars_Lev(lev, cons);
-        micro->Advance(lev, dt_advance, iteration, time, solverChoice, vars_new, z_phys_nd);
+        micro->Advance(lev, dt_advance, iteration, time, solverChoice, vars_new, z_phys_nd, phys_bc_type);
         micro->Update_State_Vars_Lev(lev, cons);
     }
 }
