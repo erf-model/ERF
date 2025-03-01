f56f8a5a7fb74ae5da27ca641d87e1ad8bc1492c
Debojyoti Ghosh <debojyoti.ghosh@gmail.com>
2025-02-28 18:34:17 -0800

    removed unnecessary vars

--- Exec/DryRegTests/ParticleAdvection/ERF_Prob.cpp
+++ Exec/DryRegTests/ParticleAdvection/ERF_Prob.cpp
@@ -55,9 +55,6 @@ Problem::init_custom_pert(
 
   AMREX_ALWAYS_ASSERT(bx.length()[2] == khi+1);
 
-  const Real rho_sfc   = p_0 / (R_d*parms.T_0);
-  //const Real thetabar  = parms.T_0;
-
   ParallelFor(bx, [=, parms_d=parms] AMREX_GPU_DEVICE(int i, int j, int k) noexcept
   {
     // Geometry (note we must include these here to get the data on device)
