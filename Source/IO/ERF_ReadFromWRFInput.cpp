e735c962d418bf9328560feef3d79d12e06aa2f3
Eliot Quon <eliot.quon@nrel.gov>
2025-03-17 15:39:35 -0600

    Cleanup mapfac read from wrfinput

--- Source/IO/ERF_ReadFromWRFInput.cpp
+++ Source/IO/ERF_ReadFromWRFInput.cpp
@@ -71,9 +71,9 @@ read_from_wrfinput (int lev,
         // Note: staggering is handled in `fill_fab_from_arrays`
         NC_dim_types.push_back(NC_Data_Dims_Type::Time_BT_SN_WE);
     }
-    else if (NC_name == "MAPFAC_UY" || NC_name == "MAPFAC_VY" || NC_name == "MAPFAC_MY" ||
-             NC_name == "MUB"       || NC_name == "SST"       || NC_name == "LANDMASK"  ||
-             NC_name == "XLAT_V" || NC_name == "XLONG_U")
+    else if (NC_name == "MAPFAC_U" || NC_name == "MAPFAC_V" || NC_name == "MAPFAC_M" ||
+             NC_name == "MUB"      || NC_name == "SST"      || NC_name == "LANDMASK"  ||
+             NC_name == "XLAT_V"   || NC_name == "XLONG_U")
     {
         // Note: staggering is handled in `fill_fab_from_arrays`
         NC_dim_types.push_back(NC_Data_Dims_Type::Time_SN_WE);
