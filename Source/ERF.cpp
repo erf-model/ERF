c48ea053ef94ce8b8dab8bdb4a04306603986b18
Ann Almgren <asalmgren@lbl.gov>
2025-03-20 15:28:57 -0700

    1) fix duplicative assert, 2) fix tagging when we might use adjacent greater e.g

--- Source/ERF.cpp
+++ Source/ERF.cpp
@@ -1760,10 +1760,6 @@ ERF::ReadParameters ()
         } // lev
     } // InitType
 
-    if (solverChoice.init_type == InitType::WRFInput) {
-        AMREX_ALWAYS_ASSERT(solverChoice.terrain_type == TerrainType::StaticFittedMesh);
-    }
-
     // What type of land surface model to use
     // NOTE: Must be checked after init_params
     if (solverChoice.lsm_type == LandSurfaceType::SLM) {
