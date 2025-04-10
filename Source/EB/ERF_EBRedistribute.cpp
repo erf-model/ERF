d88ae52cacf4d0ba09bd9ca0940981948b37c043
Soonpil Kang <spkang1123@gmail.com>
2025-04-10 14:49:58 -0700

    Removed whitespaces.

--- Source/EB/ERF_EBRedistribute.cpp
+++ Source/EB/ERF_EBRedistribute.cpp
@@ -35,10 +35,10 @@ redistribute_term ( int ncomp,
 
         EBCellFlagFab const& flagfab = ebfact.getMultiEBCellFlagFab()[mfi];
         Array4<EBCellFlag const> const& flag = flagfab.const_array();
-    
+
         bool regular = (flagfab.getType(amrex::grow(bx,4)) == FabType::regular);
         bool covered = (flagfab.getType(bx) == FabType::covered);
-    
+
         Array4<Real> out = result.array(mfi);
         Array4<Real> in  = result_tmp.array(mfi);
 
@@ -46,29 +46,29 @@ redistribute_term ( int ncomp,
         {
             auto const& vfrac = ebfact.getVolFrac().const_array(mfi);
             auto const& ccc   = ebfact.getCentroid().const_array(mfi);
-    
+
             auto const& apx = ebfact.getAreaFrac()[0]->const_array(mfi);
             auto const& apy = ebfact.getAreaFrac()[1]->const_array(mfi);
             auto const& apz = ebfact.getAreaFrac()[2]->const_array(mfi);
-    
+
             auto const& fcx = ebfact.getFaceCent()[0]->const_array(mfi);
             auto const& fcy = ebfact.getFaceCent()[1]->const_array(mfi);
             auto const& fcz = ebfact.getFaceCent()[2]->const_array(mfi);
-    
+
             Box gbx = bx; gbx.grow(3);
 
             FArrayBox scratch_fab(gbx,ncomp);
             Array4<Real> scratch = scratch_fab.array();
             Elixir eli_scratch = scratch_fab.elixir();
-    
+
             // This is scratch space if calling StateRedistribute
             // ParallelFor(Box(scratch), [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
             // {
             //     scratch(i,j,k) = 1.;
             // });
-    
+
             std::string redistribution_type = "StateRedist";
-    
+
             // State redist acts on the state.
             Array4<Real const> state_arr = state.const_array(mfi);
             ApplyRedistribution( bx, ncomp, out, in, state_arr,
@@ -113,10 +113,10 @@ redistribute_term ( int ncomp,
 
         EBCellFlagFab const& flagfab = ebfact.getMultiEBCellFlagFab()[mfi];
         Array4<EBCellFlag const> const& flag = flagfab.const_array();
-    
+
         bool regular = (flagfab.getType(amrex::grow(bx,4)) == FabType::regular);
         bool covered = (flagfab.getType(bx) == FabType::covered);
-    
+
         Array4<Real> out = result.array(mfi);
         Array4<Real> in  = result_tmp.array(mfi);
 
@@ -124,28 +124,28 @@ redistribute_term ( int ncomp,
         {
             auto const& vfrac = ebfact.getVolFrac().const_array(mfi);
             auto const& ccc   = ebfact.getCentroid().const_array(mfi);
-    
+
             auto const& apx = ebfact.getAreaFrac()[0]->const_array(mfi);
             auto const& apy = ebfact.getAreaFrac()[1]->const_array(mfi);
             auto const& apz = ebfact.getAreaFrac()[2]->const_array(mfi);
-    
+
             auto const& fcx = ebfact.getFaceCent()[0]->const_array(mfi);
             auto const& fcy = ebfact.getFaceCent()[1]->const_array(mfi);
             auto const& fcz = ebfact.getFaceCent()[2]->const_array(mfi);
-    
+
             Box bx_cc = bx;
             bx_cc = bx_cc.convert(IntVect::TheZeroVector());
             // bx_cc = bx_cc.setBig(0, bx_cc.bigEnd(0) + 1);
             Box gbx = bx_cc; gbx.grow(3);
-    
+
             FArrayBox scratch_fab(gbx,ncomp);
             Array4<Real> scratch = scratch_fab.array();
             Elixir eli_scratch = scratch_fab.elixir();
-    
+
             // This is scratch space if calling StateRedistribute
 
             std::string redistribution_type = "StateRedist";
-    
+
             // State redist acts on the state.
             Array4<Real const> state_arr = state.const_array(mfi);
             ApplyRedistribution( bx_cc, ncomp, out, in, state_arr,
