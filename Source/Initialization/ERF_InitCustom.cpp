41304f9e4d2aa9bb3442621bbbbb53f3dfd6f03d
Ann Almgren <asalmgren@lbl.gov>
2025-05-04 21:44:19 -0700

    fix typo

--- Source/Initialization/ERF_InitCustom.cpp
+++ Source/Initialization/ERF_InitCustom.cpp
@@ -61,7 +61,7 @@ ERF::init_custom (int lev)
         Array4<Real const> z_nd_arr = (z_phys_nd[lev]) ? z_phys_nd[lev]->const_array(mfi) : Array4<Real const>{};
         Array4<Real const> z_cc_arr = (z_phys_cc[lev]) ? z_phys_cc[lev]->const_array(mfi) : Array4<Real const>{};
 
-        // Here we aribtrarily choose the x-oriented map factor -- this should be generalized
+        // Here we arbitrarily choose the x-oriented map factor -- this should be generalized
         Array4<Real const> mf_m     = mapfac[lev][MapFacType::mx]->const_array(mfi);
         Array4<Real const> mf_u     = mapfac[lev][MapFacType::ux]->const_array(mfi);
         Array4<Real const> mf_v     = mapfac[lev][MapFacType::vx]->const_array(mfi);
