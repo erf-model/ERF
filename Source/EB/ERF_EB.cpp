7efc54d2807f99655ef2b87a90c5c3a426428df5
Soonpil Kang <spkang1123@gmail.com>
2025-06-18 15:21:47 -0700

    Commented unused parameter.

--- Source/EB/ERF_EB.cpp
+++ Source/EB/ERF_EB.cpp
@@ -30,7 +30,7 @@ eb_::eb_ ( )
 { }
 
 void
-eb_::make_all_factories (int level,
+eb_::make_all_factories (int /*level*/,
                          Geometry            const& a_geom,
                          BoxArray            const& ba,
                          DistributionMapping const& dm,
