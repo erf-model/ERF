03dce13b2195feb4bc7107875488a93cb849fc20
Debojyoti Ghosh <debojyoti.ghosh@gmail.com>
2025-06-19 15:44:27 -0700

    updated test tolerances to allow for differences in architectures

--- Tests/CTestList.cmake
+++ Tests/CTestList.cmake
@@ -210,9 +210,9 @@ add_test_sdm(SDM_Box3D_Cond                  "MoistRegTests/Bubble" "erf_bubble"
 # terminal velocity
 add_test_sdm(SDM_Box3D_VTerm                 "MoistRegTests/Bubble" "erf_bubble"   "plt00001" 5e-13 1e-14)
 # Congestus case
-add_test_sdm(SDM_Congestus3D                 "DevTests/TemperatureSourceSpatial"   "erf_abl_with_spatial_temperature_source" "plt00020" 5e-13 1e-14 INPUT_SOUNDING "input_sounding")
+add_test_sdm(SDM_Congestus3D                 "DevTests/TemperatureSourceSpatial"   "erf_abl_with_spatial_temperature_source" "plt00020" 5e-13 5e-13 INPUT_SOUNDING "input_sounding")
 # RICO case
-add_test_sdm(SDM_RICO3D                      "DevTests/RICO"        "erf_rico"     "plt00010" 5e-13 1e-14 INPUT_SOUNDING "input_sounding")
+add_test_sdm(SDM_RICO3D                      "DevTests/RICO"        "erf_rico"     "plt00010" 5e-13 5e-13 INPUT_SOUNDING "input_sounding")
 # multispecies setup with dummy water species
 add_test_sdm(SDM_MultiSpecies_Bubble2D       "DevTests/MultiSpeciesBubble" "erf_multispeciesbubble"     "plt00001" 5e-12 1e-12)
 
