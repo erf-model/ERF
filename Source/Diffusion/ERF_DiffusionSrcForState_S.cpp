bf1eb75d28560b920a976149bef472d7fc1f7c88
Ann Almgren <asalmgren@lbl.gov>
2025-07-06 16:23:09 -0700

    more fixes for gpu

--- Source/Diffusion/ERF_DiffusionSrcForState_S.cpp
+++ Source/Diffusion/ERF_DiffusionSrcForState_S.cpp
@@ -185,7 +185,7 @@ DiffusionSrcForState_S (const Box& bx, const Box& domain,
                                     + c2 * cell_prim(i, j, k-1, prim_index)
                                     + c3 * cell_prim(i, j, k-2, prim_index) ) );
             } else {
-                Real dzk_inv = 2.0 / (dz_ptr[k] + dz_ptr[k-1]);
+                Real dzk_inv = (k == 0) ? 1.0 / dz_ptr[k] : 2.0 / (dz_ptr[k] + dz_ptr[k-1]);
                 GradCz = dzk_inv * ( cell_prim(i, j, k, prim_index) - cell_prim(i, j, k-1, prim_index) );
             }
 
@@ -305,7 +305,7 @@ DiffusionSrcForState_S (const Box& bx, const Box& domain,
                                     + c2 * cell_prim(i, j, k-1, prim_index)
                                     + c3 * cell_prim(i, j, k-2, prim_index) ) );
             } else {
-                Real dzk_inv = 2.0 / (dz_ptr[k] + dz_ptr[k-1]);
+                Real dzk_inv = (k == 0) ? 1.0 / dz_ptr[k] : 2.0 / (dz_ptr[k] + dz_ptr[k-1]);
                 GradCz = dzk_inv * ( cell_prim(i, j, k, prim_index) - cell_prim(i, j, k-1, prim_index) );
             }
 
@@ -534,7 +534,7 @@ DiffusionSrcForState_S (const Box& bx, const Box& domain,
                                     + c2 * cell_prim(i, j, k-1, prim_index)
                                     + c3 * cell_prim(i, j, k-2, prim_index) ) );
             } else {
-                Real dzk_inv = 2.0 / (dz_ptr[k] + dz_ptr[k-1]);
+                Real dzk_inv = (k == 0) ? 1.0 / dz_ptr[k] : 2.0 / (dz_ptr[k] + dz_ptr[k-1]);
                 GradCz = dzk_inv * ( cell_prim(i, j, k, prim_index) - cell_prim(i, j, k-1, prim_index) );
             }
 
