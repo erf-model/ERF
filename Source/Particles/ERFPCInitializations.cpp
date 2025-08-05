966cc1ab33c9e5058dc13881e2b20a105d255ab2
Ann Almgren <asalmgren@lbl.gov>
2025-08-05 07:24:00 -0700

    restore original default for particle initialization

--- Source/Particles/ERFPCInitializations.cpp
+++ Source/Particles/ERFPCInitializations.cpp
@@ -116,8 +116,8 @@ void ERFPC::initializeParticlesUniformDistributionInBox (const std::unique_ptr<M
                                      height_arr(i,j+1,0) + height_arr(i+1,j+1,0) );
 
                 // SWAP THE TWO LINES BELOW TO COMPUTE ABSOLUTE HEIGHT VS HEIGHT ABOVE SURFACE
-                // Real z = zh;
-                Real z = std::max((zh-z_sfc),0.0);
+                Real z = zh;
+                // Real z = std::max((zh-z_sfc),0.0);
 
                 if (particle_init_domain.contains(RealVect(x,y,z))) {
                     num_particles_arr(i,j,k) = particles_per_cell;
