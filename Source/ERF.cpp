9a46dc4f83f3462b2be296cdc1187306167bb82a
AMLattanzi <AMLattanzi@lbl.gov>
2025-08-25 13:55:58 -0700

    Fix parenthesis.

--- Source/ERF.cpp
+++ Source/ERF.cpp
@@ -683,7 +683,7 @@ ERF::post_timestep (int nstep, Real time, Real dt_lev0)
     if (solverChoice.rad_type != RadiationType::None)
     {
         if ( rad_datalog_int > 0 &&
-             ((nstep+1) % rad_datalog_int == 0) || (nstep==0) ) {
+             (((nstep+1) % rad_datalog_int == 0) || (nstep==0)) ) {
             if (rad[0]->hasDatalog()) {
                 rad[0]->WriteDataLog(time+start_time);
             }
