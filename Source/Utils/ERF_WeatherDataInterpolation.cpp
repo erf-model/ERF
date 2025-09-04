aa707da382725f73b8fd885f2ab867ab19f157aa
Mahesh Natarajan <nataraj2@login11.chn.perlmutter.nersc.gov>
2025-09-04 05:07:25 -0700

    Correcting error

--- Source/Utils/ERF_WeatherDataInterpolation.cpp
+++ Source/Utils/ERF_WeatherDataInterpolation.cpp
@@ -358,7 +358,7 @@ ERF::InterpWeatherDataOntoMesh (const Geometry& geom_weather,
             fine_latlon_arr(i,j,k,1) = lon;
         });
 
-        ParallelFor(gtbx, gtby, gtbz,
+        ParallelFor(gtbx, gtby,
         [=] AMREX_GPU_DEVICE(int i, int j, int k) {
              // Physical location of the fine node
             Real x = prob_lo_erf[0] + i       * dx_erf[0];
