b1a07f5953ea6fd95d0a25bc5620c5240ac553a2
Aaron M. Lattanzi <103702284+AMLattanzi@users.noreply.github.com>
2025-09-22 12:16:33 -0700

    Update ERF_DiffusionSrcForMom_EB.cpp

--- Source/Diffusion/ERF_DiffusionSrcForMom_EB.cpp
+++ Source/Diffusion/ERF_DiffusionSrcForMom_EB.cpp
@@ -57,7 +57,6 @@ DiffusionSrcForMom_EB (const MFIter& mfi,
                     const eb_& ebfact,
                    [[maybe_unused]] const BCRec* d_bcrec_ptr)
 {
-#if 0
     BL_PROFILE_VAR("DiffusionSrcForMom_EB()",DiffusionSrcForMom_EB);
 
     DiffChoice dc = solverChoice.diffChoice;
@@ -357,5 +356,5 @@ DiffusionSrcForMom_EB (const MFIter& mfi,
             }
         }
     });
-#endif
+
 }
