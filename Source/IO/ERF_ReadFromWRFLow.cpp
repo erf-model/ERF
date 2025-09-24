fe3049b289fe99af3353421bf1ca624d7f64f79d
Eliot Quon <eliot.quon@nrel.gov>
2025-09-24 15:20:36 -0600

    Split read_from_wrflow into read_times_from_wrflow and read_from_wrflow(itime,...)
    following read_*_wrfbdy
    
    sst_lev and tsk_lev are resized to ntimes found in wrflowinp and only
    the first two MFs are initialized at the beginning

--- Source/IO/ERF_ReadFromWRFLow.cpp
+++ Source/IO/ERF_ReadFromWRFLow.cpp
@@ -17,16 +17,13 @@ using namespace amrex;
 #ifdef ERF_USE_NETCDF
 
 Real
-read_from_wrflow (const std::string& nc_low_file, const Box& domain,
-                  Vector<FArrayBox>& low_data_zlo,
-                  Real& start_bdy_time)
+read_times_from_wrflow (const std::string& nc_bdy_file,
+                        Vector<Vector<FArrayBox>>& low_data_zlo,
+                        Real& start_low_time)
 {
-    Print() << "Loading low boundary data from NetCDF file " << std::endl;
+    Print() << "Loading lower boundary data from NetCDF file " << std::endl;
 
     int ioproc = ParallelDescriptor::IOProcessorNumber();  // I/O rank
-
-    const auto& lo = domain.loVect();
-    const auto& hi = domain.hiVect();
 
     // *******************************************************************************
 
@@ -43,16 +40,18 @@ read_from_wrflow (const std::string& nc_low_file, const Box& domain,
         ReadNetCDFFile(nc_low_file, {"Times"}, array_ts, success);
 
         ntimes = array_ts[0].get_vshape()[0];
+
         auto dateStrLen = array_ts[0].get_vshape()[1];
         char timeStamps[ntimes][dateStrLen];
 
         // Fill up the characters read
         int str_len = static_cast<int>(dateStrLen);
-        for (int nt(0); nt < ntimes; nt++)
+        for (int nt(0); nt < ntimes; nt++) {
             for (int dateStrCt(0); dateStrCt < str_len; dateStrCt++) {
                 auto n = nt*dateStrLen + dateStrCt;
                 timeStamps[nt][dateStrCt] = *(array_ts[0].get_data() + n);
             }
+        }
 
         Vector<std::time_t> epochTimes;
         for (int nt(0); nt < ntimes; nt++) {
@@ -67,20 +66,31 @@ read_from_wrflow (const std::string& nc_low_file, const Box& domain,
                 AMREX_ALWAYS_ASSERT(epochTimes[nt] - epochTimes[nt-1] == timeInterval);
             }
         }
-        start_bdy_time = epochTimes[0];
+        start_low_time = epochTimes[0];
     }
 
-    ParallelDescriptor::Bcast(&start_bdy_time,1,ioproc);
+    ParallelDescriptor::Bcast(&start_low_time,1,ioproc);
     ParallelDescriptor::Bcast(&ntimes,1,ioproc);
     ParallelDescriptor::Bcast(&timeInterval,1,ioproc);
 
-    // *******************************************************************************
-    // Build low_data_zlo
-    // *******************************************************************************
-    Arena* Arena_Used = The_Arena();
-#ifdef AMREX_USE_GPU
-    Arena_Used = The_Pinned_Arena();
-#endif
+    // Our outermost loop is time
+    low_data_zlo.resize(ntimes);
+
+    // Make sure all processors know timeInterval
+    ParallelDescriptor::Bcast(&timeInterval,1,ioproc);
+
+    // Return the number of seconds between the boundary plane data
+    return timeInterval;
+}
+
+void
+read_from_wrflow (const int itime, const std::string& nc_low_file, const Box& domain,
+                  Vector<FArrayBox>& low_data_zlo)
+{
+    int ioproc = ParallelDescriptor::IOProcessorNumber();  // I/O rank
+                                                           //
+    const auto& lo = domain.loVect();
+    const auto& hi = domain.hiVect();
 
     IntVect plo(lo);
     IntVect phi(hi);
@@ -89,102 +99,68 @@ read_from_wrflow (const std::string& nc_low_file, const Box& domain,
     phi[0] = hi[0]; phi[1] = hi[1]; phi[2] = lo[2];
     const Box pbx_zlo(plo, phi);
 
-    for (int nt(0); nt < ntimes; ++nt) {
-        amrex::Print() << "MAKING FAB FOR LOW DATA AT TIME " << nt << " " << pbx_zlo << std::endl;
-        low_data_zlo.push_back(FArrayBox(pbx_zlo, 1, Arena_Used));
-    }
-
     // ******************************************************************
-    // Read the NetCDF file and fill this FABs
+    // Read the NetCDF file and fill this FAB
     // ******************************************************************
     Vector<std::string> nc_var_names;
     nc_var_names.push_back("SST");
 
     using RARRAY = NDArray<float>;
-    Vector<RARRAY> arrays(nc_var_names.size());
+    Vector<RARRAY> tslice(nc_var_names.size());
 
     if (ParallelDescriptor::IOProcessor())
     {
         Vector<int> success(nc_var_names.size());
 
-        for (int itime=0; itime < ntimes; ++itime)
-        {
-            Vector<RARRAY> tslice(nc_var_names.size());
+        ReadTimeSliceFromNetCDFFile(nc_low_file, itime, nc_var_names, tslice, success);
 
-            ReadTimeSliceFromNetCDFFile(nc_low_file, itime, nc_var_names, tslice, success);
-
-#if 1
-            std::vector<size_t> vshape = tslice[0].get_vshape();
-            size_t offset{1};
-            for (auto &dim:vshape) offset *= dim;
-
-            Print() << "Time " << itime << " " << nc_var_names[0] << "(";
-            for (auto &dim:vshape) {
-                amrex::Print() << dim << ",";
-            }
-            Print() << ") offset=" << offset << std::endl;
-
-            if (itime==0) {
-                // allocate full array
-                vshape[0] = ntimes;
-                arrays[0] = NDArray<float>(tslice[0].get_vname(), vshape);
-            }
-
-            float* fullPtr = arrays[0].get_data();
-            float* slicePtr = tslice[0].get_data();
-            std::copy(slicePtr, slicePtr+offset, fullPtr+itime*offset);
+        for (auto &istat:success) {
+            AMREX_ALWAYS_ASSERT(istat==1);
         }
-#endif
     }
 
+    Arena* Arena_Used = The_Arena();
+#ifdef AMREX_USE_GPU
+    Arena_Used = The_Pinned_Arena();
+#endif
 
     // *******************************************************************************
     // Now fill the data
     // *******************************************************************************
-    if (ParallelDescriptor::IOProcessor())
+    int nvar = nc_var_names.size();
+    for (int iv = 0; iv < nvar; iv++)
     {
-        Array4<Real> fab_arr;
-        int ns3 = arrays[0].get_vshape()[2];
+        low_data_zlo[itime].push_back(FArrayBox(pbx_zlo, 1, Arena_Used));
 
-        long num_pts  = low_data_zlo[0].box().numPts();
-        int ioff      = low_data_zlo[0].smallEnd()[0];
-        int joff      = low_data_zlo[0].smallEnd()[1];
-
-        for (int nt(0); nt < ntimes; ++nt)
+        if (ParallelDescriptor::IOProcessor())
         {
-            fab_arr  = low_data_zlo[nt].array();
-            int n_off = nt * num_pts;
-            for (int n(0); n < num_pts; ++n) {
-                int j  = n / ns3;
-                int i  = n - j*ns3;
-                fab_arr(ioff+i,joff+j,0) = static_cast<Real>(*(arrays[0].get_data()+n+n_off));
-            }
-        }
-    } // if ParalleDescriptor::IOProcessor()
+            Array4<Real> fab_arr = low_data_zlo[itime].array();
 
-    // *******************************************************************************
+            // dims: (Time, south_north, west_east)
+            int ns2 = tslice[iv].get_vshape()[2];
+
+            long num_pts  = low_data_zlo[0].box().numPts();
+            int ioff      = low_data_zlo[0].smallEnd()[0];
+            int joff      = low_data_zlo[0].smallEnd()[1];
+
+            for (int n(0); n < num_pts; ++n) {
+                int j  = n / ns2;
+                int i  = n - j*ns2;
+                fab_arr(ioff+i,joff+j,0) = static_cast<Real>(*(tslice[iv].get_data() + n));
+            }
+        } // if ParalleDescriptor::IOProcessor()
+    } // nc_var_names
+
     // We put a barrier here so the rest of the processors wait to do anything until they have the data
-    // *******************************************************************************
     ParallelDescriptor::Barrier();
 
-    // *******************************************************************************
     // When an FArrayBox is built, space is allocated on every rank.  However, we only
     //    filled the data in these FABs on the IOProcessor.  So here we broadcast
     //    the data to every rank.
-    // *******************************************************************************
-    for (int nt = 0; nt < ntimes; nt++)
+    for (int iv = 0; iv < nvar; iv++)
     {
-        ParallelDescriptor::Bcast(low_data_zlo[nt].dataPtr(),low_data_zlo[nt].box().numPts(),ioproc);
+        ParallelDescriptor::Bcast(low_data_zlo[itime][iv].dataPtr(),low_data_zlo[itime][iv].box().numPts(),ioproc);
     }
-
-    // Make sure all processors know how timeInterval
-    ParallelDescriptor::Bcast(&timeInterval,1,ioproc);
-
-    // Make sure all processors know how many times are stored
-    ParallelDescriptor::Bcast(&ntimes,1,ioproc);
-
-    // Return the number of seconds between the boundary plane data
-    return timeInterval;
 }
 
 #endif // ERF_USE_NETCDF
--- Source/Initialization/ERF_InitFromWRFInput.cpp
+++ Source/Initialization/ERF_InitFromWRFInput.cpp
@@ -800,26 +800,31 @@ ERF::init_from_wrfinput (int lev,
     // *******************************************************************************************
     if ((lev == 0) && !nc_low_file.empty())
     {
-        low_time_interval = read_from_wrflow(nc_low_file,geom[0].Domain(),
-                                             low_data_zlo, start_low_time);
+        low_time_interval = read_times_from_wrflow(nc_low_file,
+                                                   low_data_zlo,
+                                                   start_low_time);
 
         int i_lo = boxes_at_level[lev][0].smallEnd(0); int i_hi = boxes_at_level[lev][0].bigEnd(0);
         int j_lo = boxes_at_level[lev][0].smallEnd(1); int j_hi = boxes_at_level[lev][0].bigEnd(1);
 
         int ntimes = low_data_zlo.size();
-/*
-        // We can possibly run out of memory if we load all of wrfbdy and all of wrflow
-        // Thus we only load the first two time slices here and load more only if needed
-        ntimes = 2;
-*/
         sst_lev[lev].resize(ntimes);
         tsk_lev[lev].resize(ntimes);
 
+        // We can possibly run out of memory if we load all of wrfbdy and all of wrflow
+        // Thus we only load the first two time slices here and load more only if needed
+        ntimes = amrex::min(ntimes, 2);
+
         for (int itime(0); itime < ntimes; ++itime) {
+            read_from_wrflow(itime,nc_low_file,geom[0].Domain(),
+                             low_data_zlo,
+                             real_width);
+
             if (itime > 0) {
                 sst_lev[lev][itime] = std::make_unique<MultiFab>(ba2d[lev],dm,1,ngv);
                 tsk_lev[lev][itime] = std::make_unique<MultiFab>(ba2d[lev],dm,1,ngv);
             }
+
             for ( MFIter mfi(*(sst_lev[lev][itime]), false); mfi.isValid(); ++mfi ) {
                 Box gtbx = mfi.growntilebox();
                 FArrayBox& src = low_data_zlo[itime];
