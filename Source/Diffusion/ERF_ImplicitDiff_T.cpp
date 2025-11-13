5e6989b7c97f1727af6250ba41ce9852d1a7f4bc
Eliot Quon <eliot.quon@nrel.gov>
2025-11-13 11:07:48 -0700

    Unused var

--- Source/Diffusion/ERF_ImplicitDiff_T.cpp
+++ Source/Diffusion/ERF_ImplicitDiff_T.cpp
@@ -212,7 +212,6 @@ ImplicitDiffForMom_T (const Box& bx,
     // offsets used to average to faces
     constexpr int ioff = (stagdir == 0) ? 1 : 0;
     constexpr int joff = (stagdir == 1) ? 1 : 0;
-    constexpr int koff = (stagdir == 2) ? 1 : 0;
 
     // Box bounds
     int ilo = bx.smallEnd(0);
