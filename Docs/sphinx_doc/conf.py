a78323414ad457cc6b2e1cd78133cea32c545025
Jean M. Sexton <jmsexton@lbl.gov>
2025-11-17 14:00:00 -0800

    Changed file in wrong directory

--- Docs/sphinx_doc/conf.py
+++ Docs/sphinx_doc/conf.py
@@ -18,7 +18,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../sphinx_ext'))
+sys.path.insert(0, os.path.abspath('.'))
 #sys.path.append("../breathe")
 
 # -- General configuration ------------------------------------------------
