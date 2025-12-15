c3b4e17a838c0a0feada4a8c8f2ef7489e5d65b1
Cole Kendrick <kendrick6@llnl.gov>
2025-12-15 08:13:45 -0800

    Add interpolation for pressure grid to LSF

--- Source/ERF.cpp
+++ Source/ERF.cpp
@@ -1297,7 +1297,7 @@ ERF::InitData_post ()
     if (solverChoice.large_scale_forcing)
     {
         lsf.read_forcing_file();
-        lsf.interp_forcing(geom[0].data(), zlevels_stag[0]);
+        lsf.interp_forcing(geom[0].data(), zlevels_stag[0], input_sounding_data);
         //lsf.start_time = start_time;
     }
 
