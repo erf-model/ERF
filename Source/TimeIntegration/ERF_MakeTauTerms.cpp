9a3c7f9e544b57639d1fd30ff122e15b8d315996
Soonpil Kang <spkang1123@gmail.com>
2025-12-29 17:18:18 -0800

    Conditional divergence treatment.

--- Source/TimeIntegration/ERF_MakeTauTerms.cpp
+++ Source/TimeIntegration/ERF_MakeTauTerms.cpp
@@ -534,10 +534,16 @@ void erf_make_tau_terms (int level, int nrk,
                     });
                 } else {
                     ParallelFor(bxcc, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept {
-                        er_arr(i,j,k) = (Real(1.0)/vfrac(i,j,k)) * (
-                          dxInv[0] * ( apx(i+1,j,k)*u(i+1,j,k) - apx(i,j,k)*u(i,j,k) )
-                        + dxInv[1] * ( apy(i,j+1,k)*v(i,j+1,k) - apy(i,j,k)*v(i,j,k) )
-                        + dxInv[2] * ( apz(i,j,k+1)*w(i,j,k+1) - apz(i,j,k)*w(i,j,k) ) );
+                        if (cflag(i,j,k).isSingleValued()) {
+                            er_arr(i,j,k) = (Real(1.0)/vfrac(i,j,k)) * (
+                            dxInv[0] * ( apx(i+1,j,k)*u(i+1,j,k) - apx(i,j,k)*u(i,j,k) )
+                            + dxInv[1] * ( apy(i,j+1,k)*v(i,j+1,k) - apy(i,j,k)*v(i,j,k) )
+                            + dxInv[2] * ( apz(i,j,k+1)*w(i,j,k+1) - apz(i,j,k)*w(i,j,k) ) );
+                        } else {
+                            er_arr(i,j,k) = (u(i+1, j  , k  ) - u(i, j, k))*dxInv[0] +
+                                            (v(i  , j+1, k  ) - v(i, j, k))*dxInv[1] +
+                                            (w(i  , j  , k+1) - w(i, j, k))*dxInv[2];
+                        }
                     });
                 }
                 } // end profile
