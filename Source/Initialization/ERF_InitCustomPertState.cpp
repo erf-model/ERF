81d223ddc4183566ad7bf85e3b5c7187d14d7c5e
nataraj2 <mn556@cornell.edu>
2026-03-16 15:14:52 -0700

    Coarse basckground state to fine interpolation

--- Source/Initialization/ERF_InitCustomPertState.cpp
+++ Source/Initialization/ERF_InitCustomPertState.cpp
@@ -49,7 +49,7 @@ ERF::init_custom (int lev)
     // 4. Add the perturbations to the background state and then populate the "pert variables
 
     if(solverChoice.is_init_for_ensemble) {
-        create_background_state (); 
+        create_background_state_for_ensemble(); 
         create_random_perturbations(lev, cons_pert, xvel_pert, yvel_pert, zvel_pert);
         apply_gaussian_smoothing_to_perturbations(lev, cons_pert, xvel_pert, yvel_pert, zvel_pert);
     }
