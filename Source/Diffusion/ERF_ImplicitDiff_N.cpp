b3e228df0b64ca95837fd45d5602634c32d94b5d
AMLattanzi <AMLattanzi@lbl.gov>
2026-03-16 18:26:26 -0700

    LU decomp for theta N.

--- Source/Diffusion/ERF_ImplicitDiff_N.cpp
+++ Source/Diffusion/ERF_ImplicitDiff_N.cpp
@@ -9,6 +9,176 @@ using namespace amrex;
 
 /**
  * Function for computing the implicit contribution to the vertical diffusion
+ * of theta, with a uniform grid, no terrain, and LU decomposition.
+ *
+ * @param[in   ] bx     cell-centered box to loop over
+ * @param[in   ] level  AMR level
+ * @param[in   ] domain box of the whole domain
+ * @param[in   ] dt     time step
+ * @param[in   ] bc_neumann_vals values of derivatives if bc_type == Neumann
+ * @param[inout] cell_data conserved cell-centered rho, rho theta
+ * @param[in   ] cellSizeInv inverse cell size array
+ * @param[inout] hfx_z heat flux in z-dir
+ * @param[in   ] mu_turb turbulent viscosity
+ * @param[in   ] solverChoice container of parameters
+ * @param[in   ] bc_ptr container with boundary conditions
+ * @param[in   ] use_SurfLayer whether we have turned on subgrid diffusion
+ * @param[in   ] implicit_fac if 1 then fully implicit; if 0 then fully explicit
+ */
+void
+ImplicitDiffForStateLU_N (const Box& bx, const Box& domain,
+                          const int level,
+                          const Real dt,
+                          const GpuArray<Real, AMREX_SPACEDIM*2>& bc_neumann_vals,
+                          const Array4<      Real>& cell_data,
+                          const GpuArray<Real, AMREX_SPACEDIM>& cellSizeInv,
+                          const Array4<const Real>& hfx_z,
+                          const Array4<const Real>& mu_turb,
+                          const SolverChoice &solverChoice,
+                          const BCRec* bc_ptr,
+                          const bool use_SurfLayer,
+                          const Real implicit_fac)
+{
+    BL_PROFILE_VAR("ImplicitDiffForState_N()",ImplicitDiffForState_N);
+
+    // setup quantities for getRhoAlpha()
+#include "ERF_SetupVertDiff.H"
+    const int         n = RhoTheta_comp;
+    const int qty_index = RhoTheta_comp;
+    const int prim_index = qty_index - 1;
+    const int prim_scal_index = (qty_index >= RhoScalar_comp && qty_index < RhoScalar_comp+NSCALARS) ? PrimScalar_comp : prim_index;
+
+    // Box bounds
+    int ilo = bx.smallEnd(0);
+    int ihi = bx.bigEnd(0);
+    int jlo = bx.smallEnd(1);
+    int jhi = bx.bigEnd(1);
+    int klo = bx.smallEnd(2);
+    int khi = bx.bigEnd(2);
+    amrex::ignore_unused(ilo, ihi, jlo, jhi);
+
+    // Temporary FABs for tridiagonal solve (allocated on column)
+    //   A[k] * x[k-1] + B[k] * x[k] + C[k+1] = RHS[k]
+
+    // With LU decomposition, M * x = r is written as L * U * x = r with U * x = rho
+    // We then first have L * rho = r and U * x = rho
+    amrex::FArrayBox RHS_fab, soln_fab, coeffG_fab;
+           RHS_fab.resize(bx,1, amrex::The_Async_Arena());
+          soln_fab.resize(bx,1, amrex::The_Async_Arena());
+        coeffG_fab.resize(bx,1, amrex::The_Async_Arena());
+    auto const& RHS_a        =        RHS_fab.array();
+    auto const& soln_a       =       soln_fab.array();
+    auto const& coeffG_a     =     coeffG_fab.array();
+
+    Real dz_inv = cellSizeInv[2];
+
+    int bc_comp = qty_index;
+    bool foextrap_on_zlo = (bc_ptr[bc_comp].lo(2) == ERFBCType::foextrap);
+    bool foextrap_on_zhi = (bc_ptr[bc_comp].hi(2) == ERFBCType::foextrap);
+    bool neumann_on_zlo  = (bc_ptr[bc_comp].lo(2) == ERFBCType::neumann);
+    bool neumann_on_zhi  = (bc_ptr[bc_comp].hi(2) == ERFBCType::neumann);
+    amrex::ignore_unused(foextrap_on_zlo, foextrap_on_zhi);
+
+    AMREX_ASSERT_WITH_MESSAGE(foextrap_on_zlo || neumann_on_zlo || use_SurfLayer,
+                              "Unexpected lower BC used with implicit vertical diffusion");
+    AMREX_ASSERT_WITH_MESSAGE(foextrap_on_zhi || neumann_on_zhi,
+                              "Unexpected upper BC used with implicit vertical diffusion");
+
+    Real Fact = implicit_fac * dt * dz_inv;
+
+#ifdef AMREX_USE_GPU
+    ParallelFor(makeSlab(bx,2,0), [=] AMREX_GPU_DEVICE (int i, int j, int)
+    {
+#else
+    for (int j(jlo); j<=jhi; ++j) {
+        for (int i(ilo); i<=ihi; ++i) {
+#endif
+            // Bottom boundary coefficients and RHS for L decomp
+            //===================================================
+            Real rhoAlpha_lo, rhoAlpha_hi;
+            Real a_tmp, b_tmp, c_tmp, inv_b2_tmp;
+            {
+                getRhoAlpha(i, j, klo, rhoAlpha_lo, rhoAlpha_hi,
+                            cell_data, mu_turb, d_alpha_eff, d_eddy_diff_idz,
+                            prim_index, prim_scal_index, l_consA, l_turb);
+
+                a_tmp      = 0.;
+                c_tmp      = -Fact * rhoAlpha_hi * dz_inv;
+                b_tmp      = cell_data(i,j,klo,Rho_comp) - a_tmp - c_tmp;
+                inv_b2_tmp = 1.;
+
+                RHS_a(i,j,klo) = cell_data(i,j,klo,n); // NOTE: this is rho*theta; solution is theta
+                if (use_SurfLayer) {
+                    RHS_a(i,j,klo) +=  Fact * hfx_z(i,j,klo); // NOTE: hfx_z = -d_z(k*\theta)
+                } else if (neumann_on_zlo) {
+                    RHS_a(i,j,klo) += -Fact * rhoAlpha_lo * bc_neumann_vals[2]; // NOTE: N_val = d_z(\theta)
+                }
+
+                RHS_a(i,j,klo)    /= b_tmp;         // NOTE: this is now "rho"
+                coeffG_a(i,j,klo)  = c_tmp / b_tmp; // NOTE: this is now "gamma"
+            }
+
+            // Build the coefficients and RHS for L decomp
+            //===================================================
+            for (int k(klo+1); k < khi; k++) {
+                getRhoAlpha(i, j, k, rhoAlpha_lo, rhoAlpha_hi,
+                            cell_data, mu_turb, d_alpha_eff, d_eddy_diff_idz,
+                            prim_index, prim_scal_index, l_consA, l_turb);
+
+                a_tmp      = -Fact * rhoAlpha_lo * dz_inv;
+                c_tmp      = -Fact * rhoAlpha_hi * dz_inv;
+                b_tmp      = cell_data(i,j,k,Rho_comp) - a_tmp - c_tmp;
+                inv_b2_tmp = 1. / (b_tmp - a_tmp * coeffG_a(i,j,k-1));
+
+                RHS_a(i,j,k)    = cell_data(i,j,k,n); // NOTE: this is rho*theta; solution is theta
+
+                RHS_a(i,j,k)    = (RHS_a(i,j,k) - a_tmp * RHS_a(i,j,k-1)) * inv_b2_tmp; // NOTE: This is now "rho"
+                coeffG_a(i,j,k) = c_tmp * inv_b2_tmp; // NOTE: this is now "gamma"
+            } // k
+
+            // Top boundary coefficients and RHS for L decomp
+            //===================================================
+            {
+                getRhoAlpha(i, j, khi, rhoAlpha_lo, rhoAlpha_hi,
+                            cell_data, mu_turb, d_alpha_eff, d_eddy_diff_idz,
+                            prim_index, prim_scal_index, l_consA, l_turb);
+
+                a_tmp      = -Fact * rhoAlpha_lo * dz_inv;
+                c_tmp      = 0.;
+                b_tmp      = cell_data(i,j,khi,Rho_comp) - a_tmp - c_tmp;
+                inv_b2_tmp = 1. / (b_tmp - a_tmp * coeffG_a(i,j,khi-1));
+
+                RHS_a(i,j,khi)    = cell_data(i,j,khi,n); // NOTE: this is rho*theta; solution is theta
+                if (neumann_on_zhi) {
+                    RHS_a(i,j,khi) -= -Fact * rhoAlpha_hi * bc_neumann_vals[5]; // NOTE: N_val = d_z(\theta)
+                }
+
+                // First solve
+                soln_a(i,j,khi) = (RHS_a(i,j,khi) - a_tmp * RHS_a(i,j,khi-1)) * inv_b2_tmp;
+            }
+
+            // Back sweep the U decomp solution
+            //===================================================
+            for (int k(khi-1); k>=klo; --k) {
+                soln_a(i,j,k) = RHS_a(i,j,k) - coeffG_a(i,j,k) * soln_a(i,j,k+1);
+            }
+
+            // Convert back to rho*theta
+            //===================================================
+            for (int k(klo); k<=khi; ++k) {
+                cell_data(i,j,k,n) = cell_data(i,j,k,Rho_comp) * soln_a(i,j,k);
+            }
+
+#ifdef AMREX_USE_GPU
+    });
+#else
+      } // i
+    } // j
+#endif
+}
+
+/**
+ * Function for computing the implicit contribution to the vertical diffusion
  * of theta, with a uniform grid and no terrain.
  *
  * @param[in   ] bx     cell-centered box to loop over
