59a2b0e469199bc3c1d07145e8587903f419c31f
AMLattanzi <AMLattanzi@lbl.gov>
2026-03-17 15:38:54 -0700

    modify calday and revert to averaging.

--- Source/PhysicsInterfaces/Radiation/ERF_Radiation.cpp
+++ Source/PhysicsInterfaces/Radiation/ERF_Radiation.cpp
@@ -1086,7 +1086,7 @@ Radiation::run_impl ()
     static constexpr double dpy[] = {0.0  ,  31.0,  59.0,  90.0, 120.0, 151.0,
                                      181.0, 212.0, 243.0, 273.0, 304.0, 334.0};
     bool leap = (m_orbital_year % 4 == 0 && (!(m_orbital_year % 100 == 0) || (m_orbital_year % 400 == 0))) ? true : false;
-    double calday = dpy[m_orbital_mon-1] + (m_orbital_day-1.0) + m_orbital_sec/86400.0;
+    double calday = 1.0 + dpy[m_orbital_mon-1] + (m_orbital_day-1.0) + m_orbital_sec/86400.0;
     // add extra day if leap year and past February
     if (leap && m_orbital_mon>2) { calday += 1.0; }
     orbital_decl(calday, eccen, mvelpp, lambm0, obliqr, delta, eccf);
@@ -1163,7 +1163,8 @@ Radiation::run_impl ()
             double lon_col = h_lon(icol)*PI/180.0;
             double lcalday = calday;
             double ldelta  = delta;
-            h_mu0(icol)    = Real(orbital_cos_zenith(lcalday, lat_col, lon_col, ldelta));
+            double dt_avg  = static_cast<double>(rad_freq_in_steps) * dt;
+            h_mu0(icol)    = Real(orbital_cos_zenith(lcalday, lat_col, lon_col, ldelta, dt_avg));
         });
     }
     Kokkos::deep_copy(mu0, h_mu0);
