b8bd13c7e36ab2ad2995dc3d705d358073665679
nataraj2 <mn556@cornell.edu>
2026-03-30 14:40:11 -0700

    Some minor changes

--- Exec/ERF_Prob.cpp
+++ Exec/ERF_Prob.cpp
@@ -140,6 +140,9 @@ Problem::init_custom_pert (
     else if  (my_prob_name_ci == "supercell") {
 #include "Prob/ERF_InitCustomPert_SuperCell.H"
     }
+     else if  (my_prob_name_ci == "data_assimilation_isv") {
+#include "Prob/ERF_InitCustomPert_DataAssimilation_ISV.H"
+    }
 
     amrex::Gpu::streamSynchronize();
 }
@@ -224,6 +227,9 @@ Problem::init_custom_pert_vels (
     }
     else if (my_prob_name_ci == "userdefined") {
 #include "Prob/ERF_InitCustomPertVels_UserDefined.H"
+    }
+     else if  (my_prob_name_ci == "data_assimilation_isv") {
+#include "Prob/ERF_InitCustomPertVels_DataAssimilation_ISV.H"
     }
 
     amrex::Gpu::streamSynchronize();
