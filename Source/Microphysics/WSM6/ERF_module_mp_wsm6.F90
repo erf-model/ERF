e59c002b4c2554b54cbf4625c923094bb6547c01
Jean M. Sexton <jmsexton@lbl.gov>
2026-04-27 16:22:25 -0700

    WSM6: add canonical PSACI boundary tags for G13e

--- Source/Microphysics/WSM6/ERF_module_mp_wsm6.F90
+++ Source/Microphysics/WSM6/ERF_module_mp_wsm6.F90
@@ -1529,6 +1529,10 @@ integer:: i, j, k, mstepmax,                                     &
        write(*,'(A,I3,6E24.16)') 'WSM6-FORT_PRACI ', k, &
          praci(its,k), piacr(its,k), qr(its,k), qs(its,k), qg(its,k), qc(its,k)
      enddo
+     do k = kts, kte
+       write(*,'(A,I3,6E24.16)') 'WSM6-FORT_PSACI ', k, &
+         psaci(its,k), psacw(its,k), paacw(its,k), qs(its,k), qc(its,k), qi(its,k)
+     enddo
    endif
 !
 !
