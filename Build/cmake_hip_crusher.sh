7be4a0a2948048880c46c6273e89a396433911fb
Ann Almgren <asalmgren@lbl.gov>
2026-05-05 16:23:25 -0700

    make build scripts more consistent

--- Build/cmake_hip_crusher.sh
+++ Build/cmake_hip_crusher.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
+# Defaults (customize here or set ERF_BUILD_DIR/ERF_SOURCE_DIR/ERF_INSTALL_DIR in environment)
+# If ERF_HOME is set, use it as base for absolute paths
+if [ -n "$ERF_HOME" ]; then
+  : ${ERF_BUILD_DIR:="$ERF_HOME/build"}
+  : ${ERF_SOURCE_DIR:="$ERF_HOME"}
+  : ${ERF_INSTALL_DIR:="$ERF_HOME/install"}
+else
+  : ${ERF_BUILD_DIR:="."}
+  : ${ERF_SOURCE_DIR:=".."}
+  : ${ERF_INSTALL_DIR:="install"}
+fi
+
+echo "Source: $ERF_SOURCE_DIR | Build: $ERF_BUILD_DIR | Install: $ERF_INSTALL_DIR | PWD: $(pwd)"
+echo "Customize: export ERF_BUILD_DIR=... ERF_SOURCE_DIR=... ERF_INSTALL_DIR=... or ERF_HOME=..."
 
 module load PrgEnv-gnu craype-accel-amd-gfx90a cray-mpich rocm cmake ccache ninja git
 which clang
 which clang++
 which hipcc
-cmake -DCMAKE_INSTALL_PREFIX:PATH=./install \
+cmake -DCMAKE_INSTALL_PREFIX:PATH=$ERF_INSTALL_DIR \
       -DCMAKE_BUILD_TYPE:STRING=Release \
       -DERF_DIM:STRING=3 \
       -DERF_ENABLE_MPI:BOOL=ON \
@@ -16,4 +30,7 @@ cmake -DCMAKE_INSTALL_PREFIX:PATH=./install \
       -DCMAKE_C_COMPILER=$(which hipcc) \
       -DCMAKE_CXX_COMPILER=$(which hipcc) \
       -DCMAKE_CXX_STANDARD=17 \
-      .. && make -j8
+      --log-context --log-level STATUS \
+      -B $ERF_BUILD_DIR -S $ERF_SOURCE_DIR && \
+cmake --build $ERF_BUILD_DIR -j10 && \
+cmake --install $ERF_BUILD_DIR --prefix=$ERF_INSTALL_DIR
