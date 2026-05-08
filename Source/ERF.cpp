d111ba7c2722ae4f6eacd6dbe7d951768d5917ae
Jean M. Sexton <jmsexton@lbl.gov>
2026-05-08 11:50:10 -0700

    cleanup: remove unused BCNames in ERF.cpp

--- Source/ERF.cpp
+++ Source/ERF.cpp
@@ -129,8 +129,6 @@ Real ERF::bndry_output_planes_start_time =  zero;
 // 2D BndryRegister input
 int  ERF::input_bndry_planes             = 0;
 
-static Vector<std::string> BCNames = {"xlo", "ylo", "zlo", "xhi", "yhi", "zhi"};
-
 #ifdef ERF_USE_NETCDF
 Real read_start_time_from_wrfinput (int lev, const std::string& fname);
 Real read_start_time_from_metgrid  (int lev, const std::string& fname);
