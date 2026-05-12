ec11a655380f6951ad095487e7fbfb33f6c72387
AMLattanzi <AMLattanzi@lbl.gov>
2026-05-12 15:46:04 -0700

    add fix in other PR.

--- Source/TimeIntegration/ERF_MakeFastCoeffs.cpp
+++ Source/TimeIntegration/ERF_MakeFastCoeffs.cpp
@@ -153,8 +153,9 @@ void make_fast_coeffs (int /*level*/,
                 coeffA_a(i,j,k) = D * (one/detJ(i,j,k-1)) * ( halfg - coeff_Q * (one + RvOverRd*qv_q) * theta_t_lo );
                 coeffC_a(i,j,k) = D * (one/detJ(i,j,k  )) * (-halfg + coeff_P * (one + RvOverRd*qv_p) * theta_t_hi );
 
-                coeffB_a(i,j,k) = one + D * ( coeff_Q * (one + RvOverRd*qv_q) / detJ(i,j,k-1)
-                                            - coeff_P * (one + RvOverRd*qv_p) / detJ(i,j,k  ) ) * theta_t_mid;
+                coeffB_a(i,j,k) = one + D * ( ( coeff_Q * (one + RvOverRd*qv_q) / detJ(i,j,k-1)
+                                              - coeff_P * (one + RvOverRd*qv_p) / detJ(i,j,k  ) ) * theta_t_mid
+                                            + halfg * ( Real(1.0)/detJ(i,j,k-1) - Real(1.0)/detJ(i,j,k) ) );
             });
 
         } else {
