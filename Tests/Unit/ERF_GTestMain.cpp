cd3d1a88ba9536e6fffa484e5760e599f7c88a06
David J. Gardner <gardner48@llnl.gov>
2026-05-27 16:31:42 -0700

    workaround hang on Cray systems

--- Tests/Unit/ERF_GTestMain.cpp
+++ Tests/Unit/ERF_GTestMain.cpp
@@ -6,6 +6,16 @@ main (int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
 
+    // When invoked with --gtest_list_tests (gtest_discover_tests prints the
+    // test list this way), skip amrex::Initialize so we don't hang the MPI/GPU
+    // runtime, and use std::_Exit to skip static destructors that can block on
+    // Cray MPICH atexit handlers.
+    if (::testing::GTEST_FLAG(list_tests))
+    {
+        const int list_result = RUN_ALL_TESTS();
+        std::_Exit(list_result);
+    }
+
     amrex::Initialize(argc, argv);
     const int result = RUN_ALL_TESTS();
     amrex::Finalize();
