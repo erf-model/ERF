84469db31d0ed3f9045987752373b60c7c610b7d
AMLattanzi <AMLattanzi@lbl.gov>
2026-06-08 10:50:39 -0700

    add bndry data tend.

--- Source/Utils/ERF_InteriorGhostCells.cpp
+++ Source/Utils/ERF_InteriorGhostCells.cpp
@@ -181,12 +181,20 @@ realbdy_compute_interior_ghost_rhs (const Real& time,
     // Get bndry data
     Vector<int> ind_map2 = {BCVars::xvel_bc, BCVars::yvel_bc, BCVars::RhoTheta_bc_comp};
     Array4<Real> bdatxlo, bdatxhi, bdatylo, bdatyhi;
+    Array4<Real> btenxlo, btenxhi, btenylo, btenyhi;
     if (m_r2d) {
+        // Index is [plane orientation] and [level]
         Vector<std::unique_ptr<PlaneVector>>& bndry_data = m_r2d->interp_in_time(time);
         bdatxlo = (*bndry_data[0])[0].array();
         bdatylo = (*bndry_data[1])[0].array();
         bdatxhi = (*bndry_data[3])[0].array();
         bdatyhi = (*bndry_data[4])[0].array();
+
+        Vector<std::unique_ptr<PlaneVector>>& bndry_tend = m_r2d->get_tendency(time);
+        btenxlo = (*bndry_tend[0])[0].array();
+        btenylo = (*bndry_tend[1])[0].array();
+        btenxhi = (*bndry_tend[3])[0].array();
+        btenyhi = (*bndry_tend[4])[0].array();
     }
 
     //
@@ -553,16 +561,28 @@ realbdy_compute_interior_ghost_rhs (const Real& time,
         {
             Real rho_tend = rhs_cons(i,j,k);
             Real rho_val  = Real(0.5) * (cons_arr(i,j,k) + cons_arr(i-1,j,k));
-            Real u_tend   = (bdatxlo_np1(i,j,k) - bdatxlo_n(i,j,k)) / bdy_time_interval;
-            Real u_val    = oma * bdatxlo_n(i,j,k) + alpha * bdatxlo_np1(i,j,k);
+            Real u_tend, u_val;
+            if (btenxlo) {
+                u_tend   = btenxlo(i,j,k,BCVars::xvel_bc);
+                u_val    = bdatxlo(i,j,k,BCVars::xvel_bc);
+            } else {
+                u_tend   = (bdatxlo_np1(i,j,k) - bdatxlo_n(i,j,k)) / bdy_time_interval;
+                u_val    = oma * bdatxlo_n(i,j,k) + alpha * bdatxlo_np1(i,j,k);
+            }
                 rhs_xmom(i,j,k) = rho_val * u_tend + u_val * rho_tend;
             },
         [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
         {
             Real rho_tend = rhs_cons(i,j,k);
             Real rho_val  = Real(0.5) * (cons_arr(i,j,k) + cons_arr(i-1,j,k));
-            Real u_tend   = (bdatxhi_np1(i,j,k) - bdatxhi_n(i,j,k)) / bdy_time_interval;
-            Real u_val    = oma * bdatxhi_n(i,j,k) + alpha * bdatxhi_np1(i,j,k);
+            Real u_tend, u_val;
+            if (btenxhi) {
+                u_tend   = btenxhi(i,j,k,BCVars::xvel_bc);
+                u_val    = bdatxhi(i,j,k,BCVars::xvel_bc);
+            } else {
+                u_tend   = (bdatxhi_np1(i,j,k) - bdatxhi_n(i,j,k)) / bdy_time_interval;
+                u_val    = oma * bdatxhi_n(i,j,k) + alpha * bdatxhi_np1(i,j,k);
+            }
             rhs_xmom(i,j,k) = rho_val * u_tend + u_val * rho_tend;
         });
 
@@ -571,16 +591,28 @@ realbdy_compute_interior_ghost_rhs (const Real& time,
         {
             Real rho_tend = rhs_cons(i,j,k);
             Real rho_val  = Real(0.5) * (cons_arr(i,j,k) + cons_arr(i,j-1,k));
-            Real v_tend   = (bdatylo_np1(i,j,k) - bdatylo_n(i,j,k)) / bdy_time_interval;
-            Real v_val    = oma * bdatylo_n(i,j,k) + alpha * bdatylo_np1(i,j,k);
+            Real v_tend, v_val;
+            if (btenylo) {
+                v_tend   = btenylo(i,j,k,BCVars::yvel_bc);
+                v_val    = bdatylo(i,j,k,BCVars::yvel_bc);
+            } else {
+                v_tend   = (bdatylo_np1(i,j,k) - bdatylo_n(i,j,k)) / bdy_time_interval;
+                v_val    = oma * bdatylo_n(i,j,k) + alpha * bdatylo_np1(i,j,k);
+            }
             rhs_ymom(i,j,k) = rho_val * v_tend + v_val * rho_tend;
         },
         [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
         {
             Real rho_tend = rhs_cons(i,j,k);
             Real rho_val  = Real(0.5) * (cons_arr(i,j,k) + cons_arr(i,j-1,k));
-            Real v_tend   = (bdatyhi_np1(i,j,k) - bdatyhi_n(i,j,k)) / bdy_time_interval;
-            Real v_val    = oma * bdatyhi_n(i,j,k) + alpha * bdatyhi_np1(i,j,k);
+            Real v_tend, v_val;
+            if (btenyhi) {
+                v_tend   = btenyhi(i,j,k,BCVars::yvel_bc);
+                v_val    = bdatyhi(i,j,k,BCVars::yvel_bc);
+            } else {
+                v_tend   = (bdatyhi_np1(i,j,k) - bdatyhi_n(i,j,k)) / bdy_time_interval;
+                v_val    = oma * bdatyhi_n(i,j,k) + alpha * bdatyhi_np1(i,j,k);
+            }
             rhs_ymom(i,j,k) = rho_val * v_tend + v_val * rho_tend;
         });
     } // mfi
