bf91aff42bd00da77fcd8d9693f879c877d39cab
Ann Almgren <asalmgren@lbl.gov>
2026-07-03 15:57:07 -0700

    This PR reverts two recent changes: 1) for now we don't use erfbdy for InitType::WRFInput, and 2) we don't check for whether VariableDz can be changed to StretchedDz at start or restart

--- Source/ERF_MakeNewArrays.cpp
+++ Source/ERF_MakeNewArrays.cpp
@@ -752,10 +752,14 @@ ERF::init_zphys (int lev, Real elapsed_time)
                 AMREX_ALWAYS_ASSERT_WITH_MESSAGE(rel_diff < Real(1.e-8), "Terrain is taller than domain top!");
             }
 
+#if 0
+            // This remains commented out until we verify that the stretched and variable dz pathways
+            //   in fact give the same answer when appropriate
             if (SolverChoice::mesh_type == MeshType::VariableDz)
             {
                 check_mesh_type(lev);
             }
+#endif
         } // lev == 0
 
     } else {
