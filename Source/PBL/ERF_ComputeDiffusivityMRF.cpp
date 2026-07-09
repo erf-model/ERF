2a106977c3f2e144f41f377a2870f8ac2da87fac
Harish <harish.gopalan@nrel.gov>
2026-07-08 21:05:25 -0400

    Fix some pointers and make cases work

--- Source/PBL/ERF_ComputeDiffusivityMRF.cpp
+++ Source/PBL/ERF_ComputeDiffusivityMRF.cpp
@@ -213,7 +213,10 @@ ComputeDiffusivityMRF (const MultiFab& xvel,
         const auto& lmask_arr = (SurfLayer->get_lmask(level)) ?
                                 SurfLayer->get_lmask(level)->const_array(mfi) :
                                 Array4<int>{};
-        const Array4<Real const> z_nd_arr = z_phys_nd->array(mfi);
+        //const Array4<Real const> z_nd_arr = z_phys_nd->array(mfi);
+        // Only retrieve z_phys_nd array if terrain-fitted coordinates are in use
+        const Array4<Real const> z_nd_arr = use_terrain_fitted_coords ? z_phys_nd->array(mfi)
+                                                                : Array4<Real const>{};        
 
         //
         // PASS 1 (PREDICTOR): Compute PBL height using base surface virtual temperature.
