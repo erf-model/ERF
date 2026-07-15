d9277858ae4db9dfc6318a6ebe32ec03d15df602
Adam Sidney Wise <wise14@tuolumne1003.llnl.gov>
2026-07-15 15:08:19 -0700

    add vertical velocity perturbations

--- Source/Initialization/ERF_InitTurbPert.cpp
+++ Source/Initialization/ERF_InitTurbPert.cpp
@@ -53,3 +53,30 @@ ERF::turbPert_amplitude (int lev)
         turbPert.apply_tpi(lev, bx, RhoTheta_comp, m_ixtype, cons_pert_arr, pert_cell);
     } // mfi
 }
+
+// Calculate the perturbation region amplitude. This function is for vertical velocity perturbations.
+// This function heavily emmulates the ERF::init_custom ()
+void
+ERF::turbPert_amplitude_w (int lev)
+{
+    // Accessing data
+    auto& lev_new = vars_new[lev];
+
+    // Creating local data
+    int ncomps = lev_new[Vars::zvel].nComp();
+    MultiFab wvel_data(lev_new[Vars::zvel], make_alias, 0, ncomps);
+
+    // Defining BoxArray type
+    auto m_ixtype = wvel_data.boxArray().ixType();
+
+#ifdef _OPENMP
+#pragma omp parallel if (amrex::Gpu::notInLaunchRegion())
+#endif
+    for (MFIter mfi(lev_new[Vars::zvel], TileNoZ()); mfi.isValid(); ++mfi) {
+        const Box &bx  = mfi.validbox();
+        const auto &wvel_pert_arr = wvel_data.array(mfi); // Address of perturbation array
+        const amrex::Array4<const amrex::Real> &pert_cell = turbPert.pb_cell[lev].array(mfi); // per-cell perturbation stored in structure
+
+        turbPert.apply_tpi(lev, bx, -1, m_ixtype, wvel_pert_arr, pert_cell);
+    } // mfi
+}
