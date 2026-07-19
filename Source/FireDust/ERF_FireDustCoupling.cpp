f0f281969097bdda2de06fa0700bf0479e03f15c
Harish <harish.gopalan@nrel.gov>
2026-07-18 20:08:07 -0600

    Fix: remove AMReX_ParallelFor.H and use amrex::Loop instead

--- Source/FireDust/ERF_FireDustCoupling.cpp
+++ Source/FireDust/ERF_FireDustCoupling.cpp
@@ -5,7 +5,7 @@
 #include <AMReX_MultiFab.H>
 #include <AMReX_Geometry.H>
 #include <AMReX_MFIter.H>
-#include <AMReX_ParallelFor.H>
+#include <AMReX_Loop.H>
 
 using namespace amrex;
 
@@ -23,12 +23,12 @@ void FireDustCoupling::apply_burned_area_to_crust(
         Array4<Real> crust = dust_crust_index.array(mfi);
         Array4<const Real> fire_phi = fire_phi_mf->const_array(mfi);
 
-        ParallelFor(bx, [=] AMREX_GPU_DEVICE (const IntVect& iv) {
+        amrex::Loop(bx, [=] (int i, int j, int k) noexcept {
             // Check if cell has been burned (fire_phi < 0)
-            if (fire_phi(iv) < 0.0_rt) {
+            if (fire_phi(i, j, k) < 0.0_rt) {
                 // Apply crust reduction: crust *= (1 - reduction)
-                crust(iv) *= (1.0_rt - post_fire_crust_reduction);
-                crust(iv) = amrex::max(crust(iv), 0.0_rt);
+                crust(i, j, k) *= (1.0_rt - post_fire_crust_reduction);
+                crust(i, j, k) = amrex::max(crust(i, j, k), 0.0_rt);
             }
         });
     }
