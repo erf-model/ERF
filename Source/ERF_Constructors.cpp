0ffd50b05aff49422f892741f99d8e05116247c3
Soonpil Kang <spkang1123@gmail.com>
2026-07-27 21:39:15 -0700

    Add USE_FC_FACTORY around EB2::BuildFC().

--- Source/ERF_Constructors.cpp
+++ Source/ERF_Constructors.cpp
@@ -450,7 +450,9 @@ ERF::ERF_shared ()
                             ngrow_for_eb, build_coarse_level_by_coarsening);
             } else {
                 EB2::Build(gshop, this->Geom(), ngrow_for_eb);
+#if USE_FC_FACTORY
                 EB2::BuildFC();
+#endif
             }
         } else if (geometry == "plane") {
             RealArray plane_point{zero, zero, zero};
@@ -464,7 +466,9 @@ ERF::ERF_shared ()
                             ngrow_for_eb, build_coarse_level_by_coarsening);
             } else {
                 EB2::Build(gshop, this->Geom(), ngrow_for_eb);
+#if USE_FC_FACTORY
                 EB2::BuildFC();
+#endif
             }
         } else if (geometry == "box") {
             RealArray box_lo{zero, zero, zero};
@@ -478,7 +482,9 @@ ERF::ERF_shared ()
                             ngrow_for_eb, build_coarse_level_by_coarsening);
             } else {
                 EB2::Build(gshop, this->Geom(), ngrow_for_eb);
+#if USE_FC_FACTORY
                 EB2::BuildFC();
+#endif
             }
         } else if (geometry == "sphere") {
             auto ProbLoArr = geom[max_level].ProbLoArray();
@@ -493,7 +499,9 @@ ERF::ERF_shared ()
                             ngrow_for_eb, build_coarse_level_by_coarsening);
             } else {
                 EB2::Build(gshop, this->Geom(), ngrow_for_eb);
+#if USE_FC_FACTORY
                 EB2::BuildFC();
+#endif
             }
         }
     }
