a18a3a3ceacab761c6238150de91c532d2156e26
Ann Almgren <asalmgren@lbl.gov>
2026-08-03 19:32:56 -0700

    Add doxygen comments only; no uncommented line changes (#3528)

--- Source/Advection/ERF_AdvectionSrcForOpenBC.cpp
+++ Source/Advection/ERF_AdvectionSrcForOpenBC.cpp
@@ -2,7 +2,17 @@
 
 using namespace amrex;
 
-/** Compute advection tendencies for momentum normal to BC */
+/**
+ * Compute advection tendencies for momentum normal to an open boundary.
+ *
+ * @param[in] bx box over which the boundary-normal momentum is updated
+ * @param[in] dir coordinate direction normal to the open boundary
+ * @param[out] rhs_arr tendency for the boundary-normal momentum equation
+ * @param[in] vel_norm_arr velocity component normal to the open boundary
+ * @param[in] cell_data_arr cell-centered conservative state
+ * @param[in] dxInv inverse mesh spacing
+ * @param[in] do_lo flag for a low-side open boundary
+ */
 void
 AdvectionSrcForOpenBC_Normal (const Box& bx,
                               const int& dir,
@@ -35,7 +45,22 @@ AdvectionSrcForOpenBC_Normal (const Box& bx,
     });
 }
 
-/** Compute advection tendencies for x momentum tangential to BC (2nd order)*/
+/**
+ * Compute second-order advection tendencies for x-momentum tangent to an open boundary.
+ *
+ * @param[in] bxx box over which x-momentum is updated
+ * @param[in] dir coordinate direction normal to the open boundary
+ * @param[out] rho_u_rhs tendency for the x-momentum equation
+ * @param[in] u x-component of velocity
+ * @param[in] rho_u x-component of momentum
+ * @param[in] rho_v y-component of momentum
+ * @param[in] Omega component of momentum normal to the z-coordinate surface
+ * @param[in] ax area fraction of x-faces
+ * @param[in] az area fraction of z-faces
+ * @param[in] detJ Jacobian of the metric transformation
+ * @param[in] cellSizeInv inverse mesh spacing
+ * @param[in] do_lo flag for a low-side open boundary
+ */
 void
 AdvectionSrcForOpenBC_Tangent_Xmom (const Box& bxx,
                                     const int& dir,
@@ -75,7 +100,22 @@ AdvectionSrcForOpenBC_Tangent_Xmom (const Box& bxx,
     });
 }
 
-/** Compute advection tendencies for x momentum tangential to BC (2nd order)*/
+/**
+ * Compute second-order advection tendencies for y-momentum tangent to an open boundary.
+ *
+ * @param[in] bxy box over which y-momentum is updated
+ * @param[in] dir coordinate direction normal to the open boundary
+ * @param[out] rho_v_rhs tendency for the y-momentum equation
+ * @param[in] v y-component of velocity
+ * @param[in] rho_u x-component of momentum
+ * @param[in] rho_v y-component of momentum
+ * @param[in] Omega component of momentum normal to the z-coordinate surface
+ * @param[in] ay area fraction of y-faces
+ * @param[in] az area fraction of z-faces
+ * @param[in] detJ Jacobian of the metric transformation
+ * @param[in] cellSizeInv inverse mesh spacing
+ * @param[in] do_lo flag for a low-side open boundary
+ */
 void
 AdvectionSrcForOpenBC_Tangent_Ymom (const Box& bxy,
                                     const int& dir,
@@ -115,7 +155,24 @@ AdvectionSrcForOpenBC_Tangent_Ymom (const Box& bxy,
     });
 }
 
-/** Compute advection tendencies for x momentum tangential to BC (2nd order)*/
+/**
+ * Compute second-order advection tendencies for z-momentum tangent to an open boundary.
+ *
+ * @param[in] bxz box over which z-momentum is updated
+ * @param[in] dir coordinate direction normal to the open boundary
+ * @param[out] rho_w_rhs tendency for the z-momentum equation
+ * @param[in] w z-component of velocity
+ * @param[in] rho_u x-component of momentum
+ * @param[in] rho_v y-component of momentum
+ * @param[in] Omega component of momentum normal to the z-coordinate surface
+ * @param[in] ax area fraction of x-faces
+ * @param[in] ay area fraction of y-faces
+ * @param[in] az area fraction of z-faces
+ * @param[in] detJ Jacobian of the metric transformation
+ * @param[in] cellSizeInv inverse mesh spacing
+ * @param[in] domhi_z maximum cell-centered k-index in the domain
+ * @param[in] do_lo flag for a low-side open boundary
+ */
 void
 AdvectionSrcForOpenBC_Tangent_Zmom (const Box& bxz,
                                     const int& dir,
@@ -175,7 +232,22 @@ AdvectionSrcForOpenBC_Tangent_Zmom (const Box& bxz,
     });
 }
 
-/** Compute advection tendencies for x momentum tangential to BC (2nd order)*/
+/**
+ * Compute second-order advection tendencies for conserved variables tangent to an open boundary.
+ *
+ * @param[in] bx box over which conserved variables are updated
+ * @param[in] dir coordinate direction normal to the open boundary
+ * @param[in] icomp first conserved component to update
+ * @param[in] ncomp number of conserved components to update
+ * @param[out] cell_rhs tendency for conserved variables
+ * @param[in] cell_prim primitive scalar variables
+ * @param[in] avg_xmom x-component of time-averaged momentum
+ * @param[in] avg_ymom y-component of time-averaged momentum
+ * @param[in] avg_zmom z-component of time-averaged momentum
+ * @param[in] detJ Jacobian of the metric transformation
+ * @param[in] cellSizeInv inverse mesh spacing
+ * @param[in] do_lo flag for a low-side open boundary
+ */
 void
 AdvectionSrcForOpenBC_Tangent_Cons (const Box& bx,
                                     const int& dir,
@@ -233,6 +305,20 @@ AdvectionSrcForOpenBC_Tangent_Cons (const Box& bx,
 }
 
 
+/**
+ * Compute the open-boundary normal-direction source for a tangential primitive variable.
+ *
+ * @param[in] i i-index of the target cell
+ * @param[in] j j-index of the target cell
+ * @param[in] k k-index of the target cell
+ * @param[in] nprim primitive component to update
+ * @param[in] dir coordinate direction normal to the open boundary
+ * @param[in] prim_tang_arr primitive variable tangent to the open boundary
+ * @param[in] mom_norm_arr momentum component normal to the open boundary
+ * @param[in] dxInv inverse mesh spacing in the open-boundary normal direction
+ * @param[in] do_lo flag for a low-side open boundary
+ * @return source contribution before the wrapper applies its sign convention
+ */
 AMREX_FORCE_INLINE
 AMREX_GPU_HOST_DEVICE
 Real
