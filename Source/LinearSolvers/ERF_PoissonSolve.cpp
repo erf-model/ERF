e664bc5f58b5fb15e6927b9dec0a7064d443e927
Ann Almgren <asalmgren@lbl.gov>
2026-08-09 16:31:29 -0700

    more complete fix for these issues

--- Source/LinearSolvers/ERF_PoissonSolve.cpp
+++ Source/LinearSolvers/ERF_PoissonSolve.cpp
@@ -240,6 +240,13 @@ void ERF::project_momenta (int lev, double l_time, double l_dt_d, Vector<MultiFa
         } else {
             fluxes[0][idim].define(convert(ba_tmp[0], IntVect::TheDimensionVector(idim)), dm_tmp[0], 1, 0);
         }
+        //
+        // A subdomain whose RHS is already below poisson_abstol is skipped below, so its
+        // fluxes are never written by a solve. They are still added to the momenta after
+        // the loop, so they must start at zero (FArrayBox data is uninitialized unless
+        // AMREX_DEBUG is set).
+        //
+        fluxes[0][idim].setVal(0.0);
     }
 
     // ****************************************************************************
