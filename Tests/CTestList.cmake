fa7cadd0ea1e7601aa518ae77864e3388e6ca40c
Cole Kendrick <kendrick6@llnl.gov>
2026-08-15 08:43:03 -0700

    Skip test if not built with FFT enabled

--- Tests/CTestList.cmake
+++ Tests/CTestList.cmake
@@ -668,7 +668,9 @@ add_test_r(ABL_MOST_IMP_DIFF_TKE
     "erf_exec"
     "plt00010"
     FCOMPARE_ATOL "4.0e-10")
-add_test_r(ABL_MOST_Cloudchamber             ""  "erf_exec" "plt00010")
+if(ERF_ENABLE_FFT)
+    add_test_r(ABL_MOST_Cloudchamber         ""  "erf_exec" "plt00010")
+endif()
 add_test_r(ABL_MYNN_PBL                      ""  "erf_exec" "plt00100" INPUT_SOUNDING "input_sounding_GABLS1" RUNTIME_OPTIONS "erf.vert_implicit=false " )
 add_test_r(ABL_InflowFile                    ""  "erf_exec" "plt00010" RUNTIME_OPTIONS "erf.vert_implicit=false ")
 add_test_r(MoistBubble                       ""  "erf_exec" "plt00010" RUNTIME_OPTIONS "erf.vert_implicit=false ")
