9f246cc2c19653a6c4d4dabe3c1bf2f963af59eb
nataraj2 <mn556@cornell.edu>
2026-09-11 16:17:56 -0700

    Udpating station tracker

--- Source/ERF.cpp
+++ Source/ERF.cpp
@@ -596,8 +596,15 @@ ERF::post_timestep (int nstep, double time, double dt_lev0)
         }
     }
 
-    WeatherDiagnosticsTracker(solverChoice); 
+    if(nstep == 0 or nstep%solverChoice.station_plot_freq == 0) {
+        WeatherDiagnosticsTracker(solverChoice);
 
+        if (ParallelDescriptor::IOProcessor()) {
+            MakeStationTrackerDirectory("StationTracker");
+            std::string filename_station_tracker = MakeStationTrackerFilename("rain_accum",nstep);
+            WriteLinePlot(filename_station_tracker, station_rain_accum_vs_time);
+        }
+    }
 } // post_timestep
 
 // This is called from main.cpp and handles all initialization, whether from start or restart
